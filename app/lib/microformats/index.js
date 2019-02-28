f0dc3a016456751807e0674052f31ff356034b2c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-02-28 15:29:10 +0000

    Simplify post-type discovery

--- app/lib/microformats/index.js
+++ app/lib/microformats/index.js
@@ -8,7 +8,6 @@ module.exports = {
   derivePhotoProperty: require(process.env.PWD + '/app/lib/microformats/derive-photo-property'),
   derivePuplishedProperty: require(process.env.PWD + '/app/lib/microformats/derive-published-property'),
   deriveSlug: require(process.env.PWD + '/app/lib/microformats/derive-slug'),
-  deriveType: require(process.env.PWD + '/app/lib/microformats/derive-type'),
   formEncodedToMf2: require(process.env.PWD + '/app/lib/microformats/form-encoded-to-mf2'),
   htmlToMf2: require(process.env.PWD + '/app/lib/microformats/html-to-mf2'),
   urlToMf2: require(process.env.PWD + '/app/lib/microformats/url-to-mf2')
