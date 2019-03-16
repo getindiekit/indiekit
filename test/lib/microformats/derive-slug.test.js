b4a65b8e3173d51a5a29c603fcd6d3c23c6f2b59
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-03-16 23:35:19 +0000

    Ignore empty mp-slug values

--- test/lib/microformats/derive-slug.test.js
+++ test/lib/microformats/derive-slug.test.js
@@ -5,6 +5,7 @@ const derviveSlug = require(process.env.PWD + '/app/lib/microformats/derive-slug
 
 // Fixtures
 const provided = require('./fixtures/slug-provided');
+const providedEmpty = require('./fixtures/slug-provided-empty');
 const missing = require('./fixtures/slug-missing');
 const missingNoName = require('./fixtures/slug-missing-no-name');
 
@@ -12,6 +13,11 @@ const missingNoName = require('./fixtures/slug-missing-no-name');
 test('Derives slug from `mp-slug` property', t => {
   const slug = derviveSlug(provided, '-');
   t.is(slug[0], 'made-a-thing');
+});
+
+test('Derives slug, ignoring empty `mp-slug` property', t => {
+  const slug = derviveSlug(providedEmpty, '-');
+  t.is(slug[0], 'made-a-thing-with-javascript');
 });
 
 test('Derives slug from `name` property', t => {
