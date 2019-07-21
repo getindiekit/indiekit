eb564e9252175b4bc46dfc7c8228e472bebd9778
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-07-21 20:38:46 +0100

    Refactor micropub query as Express middleware

---
+++ app/lib/publication/get-post-types.js
@@ -0,0 +1,24 @@
+/**
+ * Returns an array of supported post types
+ *
+ * @memberof micropub
+ * @module getPostTypes
+ * @param {String} pub Publication configuration
+ * @returns {Array} Array of post types
+ */
+module.exports = pub => {
+  const pubPostTypes = pub['post-types'];
+  const postTypes = [];
+
+  if (pubPostTypes) {
+    for (const [key, value] of Object.entries(pubPostTypes)) {
+      const postType = {
+        type: key,
+        name: value.name
+      };
+      postTypes.push(postType);
+    }
+  }
+
+  return postTypes;
+};
