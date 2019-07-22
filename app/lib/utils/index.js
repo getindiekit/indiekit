02f5455d7e2648b4066dba2e27e2c29088f9ef33
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-07-22 20:11:33 +0100

    Remove deprecated error and success utilities

--- app/lib/utils/index.js
+++ app/lib/utils/index.js
@@ -89,54 +89,6 @@ const decodeFormEncodedString = str => {
 };
 
 /**
- * Returns error information
- *
- * @memberof utils
- * @module error
- * @param {String} id Identifier
- * @param {String} desc Description
- * @returns {Object} Error object
- */
-const error = (id, desc) => {
-  let code;
-
-  switch (id) {
-    case ('not_found'):
-      code = 404;
-      desc = desc || 'Resource not found';
-      break;
-    case ('forbidden'):
-      code = 403;
-      desc = desc || 'User does not have permission to perform request';
-      break;
-    case ('unauthorized'):
-      code = 401;
-      desc = desc || 'No access token provided in request';
-      break;
-    case ('insufficient_scope'):
-      code = 401;
-      desc = desc || 'Scope of access token does not meet requirements for request';
-      break;
-    case ('invalid_request'):
-      code = 400;
-      desc = desc || 'Request is missing required parameter, or there was a problem with value of one of the parameters provided';
-      break;
-    default:
-      id = 'server_error';
-      code = 500;
-      desc = desc || 'Server error';
-  }
-
-  return {
-    code,
-    body: {
-      error: id,
-      error_description: desc
-    }
-  };
-};
-
-/**
  * Gets first n words from a string
  *
  * @memberof utils
@@ -166,66 +118,11 @@ const normalizePath = str => {
   return str.replace(/^\/|\/$/g, '');
 };
 
-/**
- * Returns success information
- *
- * @memberof micropub
- * @module response
- * @param {String} id Identifier
- * @param {String} location Location of post
- * @returns {Object} Success object
- */
-const success = (id, location) => {
-  let code;
-  let desc;
-
-  switch (id) {
-    case ('create'):
-      code = 201;
-      desc = `Post created at ${location}`;
-      break;
-    case ('create_pending'):
-      code = 202;
-      desc = `Post will be created at ${location}`;
-      break;
-    case ('update'):
-      code = 200;
-      desc = `Post updated at ${location}`;
-      break;
-    case ('update_created'):
-      code = 201;
-      desc = `Post updated and moved to ${location}`;
-      break;
-    case ('delete'):
-      code = 200;
-      desc = `Post deleted from ${location}`;
-      break;
-    case ('delete_undelete'):
-      code = 201;
-      desc = `Post undeleted from ${location}`;
-      break;
-    default:
-      code = 200;
-      desc = 'Success';
-  }
-
-  return {
-    code,
-    location,
-    body: {
-      success: id,
-      success_description: desc
-    }
-  };
-};
-
 module.exports = {
   createRandomString,
   decodeFormEncodedString,
   deriveFileProperties,
   deriveMediaType,
-  error,
   excerptString,
-  normalizePath,
-  success
+  normalizePath
 };
