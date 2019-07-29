9237d308d920c536424ddcbba0f4444a52487a4d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-07-29 23:29:15 +0100

    Fix #36, resolving publisher configuration when no custom template(s) provided.

--- app/lib/publication/resolve-config.js
+++ app/lib/publication/resolve-config.js
@@ -19,60 +19,65 @@ const appConfig = require('./defaults');
 module.exports = async configPath => {
   // If no configuration provided, use application defaults
   if (!configPath) {
-    logger.info('publication.resolveConfig: No configuration provided. Using app defaults.');
+    logger.info('publication.resolveConfig: No configuration provided. Using app defaults', {appConfig});
     return appConfig;
   }
 
+  let pubConfig;
   try {
-    // Fetch configuration from store and cache
-    let pubConfig = await cache.read(configPath, 'config.json');
-
-    // If no configuration found in store, use application defaults
-    if (pubConfig) {
-      pubConfig = JSON.parse(pubConfig);
-      logger.info('publication.resolveConfig, using cached', {pubConfig});
-    } else {
-      logger.info('publication.resolveConfig, using defaults', {appConfig});
-      return appConfig;
-    }
-
-    // Merge store configuration with application defaults
-    const newConfig = _.merge(appConfig, pubConfig);
-
-    // Fetch templates from store and cache
-    let newPostTypes;
-    const appPostTypes = appConfig['post-types'];
-    const pubPostTypes = pubConfig['post-types'];
-
-    if (pubPostTypes) {
-      const cacheTemplates = [];
-      for (const key in pubPostTypes) {
-        if ({}.hasOwnProperty.call(pubPostTypes, key)) {
-          const postType = pubPostTypes[key];
-          const cacheTemplate = path.join('templates', `${key}.njk`);
-          const cacheTemplatePath = path.join(config.cache.dir, cacheTemplate);
-          cacheTemplates.push(
-            cache.read(postType.template, cacheTemplate)
-          );
-
-          // Update `template` value with location of cached template
-          postType.template = cacheTemplatePath;
-        }
-      }
-
-      await Promise.all(cacheTemplates);
-
-      // Merge default and publication post types (with cached template paths)
-      newPostTypes = _.merge(appPostTypes, pubPostTypes);
-
-      // Update combined configuration with new post type values
-      newConfig['post-types'] = newPostTypes;
-    }
-
-    logger.info('publication.resolveConfig, resolved', {newConfig});
-    return newConfig;
+    // Fetch publisher configuration from store and cache
+    pubConfig = await cache.read(configPath, 'config.json');
   } catch (error) {
     logger.error('publication.resolveConfig', {error});
     throw new Error(error);
   }
+
+  // If no configuration found in store, use application defaults
+  if (pubConfig) {
+    pubConfig = JSON.parse(pubConfig);
+    logger.info('publication.resolveConfig, using cached', {pubConfig});
+  } else {
+    logger.info('publication.resolveConfig: Configuration not found. Using app defaults', {appConfig});
+    return appConfig;
+  }
+
+  // Merge publisher configuration with application defaults
+  const newConfig = _.merge(appConfig, pubConfig);
+
+  // Fetch publisher post type templates from store and cache
+  let newPostTypes;
+  const appPostTypes = appConfig['post-types'];
+  const pubPostTypes = pubConfig['post-types'];
+
+  if (pubPostTypes) {
+    const cacheTemplates = [];
+    for (const key in pubPostTypes) {
+      if (Object.prototype.hasOwnProperty.call(pubPostTypes, key)) {
+        const pubPostType = pubPostTypes[key];
+        // Only cache templates publisher has configured
+        if (pubPostType.template) {
+          const cacheTemplate = path.join('templates', `${key}.njk`);
+          const cacheTemplatePath = path.join(config.cache.dir, cacheTemplate);
+          cacheTemplates.push(
+            cache.read(pubPostType.template, cacheTemplate)
+          );
+
+          // Update `template` value with location of cached template
+          pubPostType.template = cacheTemplatePath;
+        }
+      }
+    }
+
+    // Wait for all templates to be cached
+    await Promise.all(cacheTemplates);
+
+    // Merge default and publication post types (with cached template paths)
+    newPostTypes = _.merge(appPostTypes, pubPostTypes);
+
+    // Update combined configuration with new post type values
+    newConfig['post-types'] = newPostTypes;
+  }
+
+  logger.info('publication.resolveConfig, resolved', {newConfig});
+  return newConfig;
 };
