68bc74c73c6673aa684adedb83a493947bf50bb0
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2019-08-12 21:15:47 +0100

    Update tests

---
+++ test/lib/errors.test.js
@@ -0,0 +1,20 @@
+const test = require('ava');
+
+const {IndieKitError} = require(process.env.PWD + '/lib/errors');
+
+test('Throws errror', t => {
+  const fn = () => {
+    throw new IndieKitError({
+      error: 'teapot',
+      error_description: 'I’m a teapot'
+    });
+  };
+
+  const error = t.throws(() => {
+    fn();
+  }, IndieKitError);
+
+  t.log(error);
+  t.is(error.message.error, 'teapot');
+  t.is(error.message.error_description, 'I’m a teapot');
+});
