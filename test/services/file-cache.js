a5c34517dd4ab01235bc5078c254246736146840
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-05-17 11:58:03 +0100

    test: mock requests

--- test/services/file-cache.js
+++ test/services/file-cache.js
@@ -1,26 +1,41 @@
 import test from 'ava';
+import nock from 'nock';
+
 import {client} from '../../config/server.js';
 import fileCacheService from '../../services/file-cache.js';
+
+test.beforeEach(t => {
+  t.context = {
+    nock: nock('https://website.example').get('/categories.json'),
+    url: 'https://website.example/categories.json'
+  };
+});
 
 test.afterEach(() => {
   client.flushall();
 });
 
 test('Gets data from remote and saves to Redis cache', async t => {
-  const url = 'https://paulrobertlloyd.com/categories/index.json';
-  const result = await fileCacheService('category', url);
+  const scope = t.context.nock.reply(200, ['Foo', 'Bar']);
+  const result = await fileCacheService('category', t.context.url);
+
   t.is(result.source, 'fetch');
+  scope.done();
 });
 
 test('Throws error if remote not found', async t => {
-  const url = null;
-  const error = await t.throwsAsync(fileCacheService('file', url));
-  t.regex(error.message, /\bReceived null\b/);
+  const scope = t.context.nock.replyWithError('not found');
+  const error = await t.throwsAsync(fileCacheService('file', t.context.url));
+
+  t.is(error.message, 'not found');
+  scope.done();
 });
 
 test('Gets data from Redis cache', async t => {
-  const url = 'https://paulrobertlloyd.com/categories/index.json';
-  await fileCacheService('file', url);
-  const result = await fileCacheService('file', url);
+  const scope = t.context.nock.reply(200, ['Foo', 'Bar']);
+  await fileCacheService('file', t.context.url);
+  const result = await fileCacheService('file', t.context.url);
+
   t.is(result.source, 'cache');
+  scope.done();
 });
