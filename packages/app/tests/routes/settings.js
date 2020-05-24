f134145decfa5687d2a632a367448b31ed0a45b2
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-05-24 01:48:10 +0100

    feat: save application settings

--- packages/app/tests/routes/settings.js
+++ packages/app/tests/routes/settings.js
@@ -6,10 +6,13 @@ import {client} from '../../config/db.js';
 
 const request = supertest(app);
 
+<<<<<<< HEAD
 test.afterEach(() => {
   client.flushall();
 });
 
+=======
+>>>>>>> 678c0db... feat: save application settings
 test('Gets all settings', async t => {
   const response = await request.get('/settings');
   t.is(response.status, 200);
