22b6266737e4a09f0dca0cd6f2ae8e3b0db4d1c4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-05-25 18:43:35 +0100

    test: home and sign-in routes

---
+++ packages/app/tests/routes/sign-in.js
@@ -0,0 +1,12 @@
+import test from 'ava';
+import supertest from 'supertest';
+
+import app from '../../index.js';
+const request = supertest(app);
+
+test('Returns homepage', async t => {
+  const response = await request.get('/sign-in');
+  t.is(response.status, 200);
+  t.is(response.type, 'text/html');
+});
+
