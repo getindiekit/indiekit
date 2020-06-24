047d80349d5204e37e72ed0abaeca4f22b5e4fde
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-06-24 22:29:02 +0100

    feat(indiekit): post to micropub endpoint from share sheet

--- packages/indiekit/controllers/share.js
+++ packages/indiekit/controllers/share.js
@@ -1,3 +1,5 @@
+import got from 'got';
+
 export const editShare = (request, response) => {
   const {content, name, url, success} = request.query;
 
@@ -9,4 +11,28 @@ export const editShare = (request, response) => {
     success,
     minimalui: (request.params.path === 'bookmarklet')
   });
+};
+
+export const saveShare = async (request, response, next) => {
+  const {endpoints} = response.locals.application;
+  const micropubEndpoint = endpoints.find(
+    endpoint => endpoint.id === 'micropub'
+  );
+  const host = `${request.protocol}://${request.headers.host}`;
+  const path = micropubEndpoint.mountpath;
+
+  try {
+    const postResponse = await got.post(`${host}${path}`, {
+      form: request.body,
+      responseType: 'json'
+    });
+
+    const success = postResponse.body;
+    if (success) {
+      const message = encodeURIComponent(success.description);
+      response.redirect(`?success=${message}`);
+    }
+  } catch (error) {
+    next(error);
+  }
 };
--- packages/indiekit/routes/share.js
+++ packages/indiekit/routes/share.js
@@ -4,5 +4,6 @@ import * as share from '../controllers/share.js';
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.get('/:path?', share.editShare);
+router.post('/:path?', share.saveShare);
 
 export const shareRoutes = router;
