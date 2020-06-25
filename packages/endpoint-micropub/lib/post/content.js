d115fdd9a07bffa6d863376849ecd238dcf96ce3
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-06-25 23:13:42 +0100

    fix(endpoint-micropub): camelcase property keys

--- packages/endpoint-micropub/lib/post/content.js
+++ packages/endpoint-micropub/lib/post/content.js
@@ -1,10 +1,12 @@
 import fs from 'fs';
+import camelcaseKeys from 'camelcase-keys';
 import {templates} from '../nunjucks.js';
 
 export const createPostContent = (postData, postTemplatePath) => {
   try {
     // Derive properties
-    const {properties} = postData.mf2;
+    let {properties} = postData.mf2;
+    properties = camelcaseKeys(properties);
 
     // Prepare content
     const template = fs.readFileSync(postTemplatePath, 'utf-8');
