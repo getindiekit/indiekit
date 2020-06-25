6437e8a970482ca93bddb86327b32ab58bc8e9a6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-06-25 22:28:24 +0100

    refactor: add error handling to database config

--- packages/indiekit/config/database.js
+++ packages/indiekit/config/database.js
@@ -1,6 +1,14 @@
 import Redis from 'ioredis';
 
-export const databaseConfig = {
-  client: new Redis(process.env.REDIS_URL),
-  expires: 3600
-};
+export const databaseConfig = (() => {
+  try {
+    return {
+      client: new Redis(process.env.REDIS_URL, {
+        showFriendlyErrorStack: process.env.NODE_ENV === 'development'
+      }),
+      expires: 3600
+    };
+  } catch (error) {
+    throw new Error(error.message);
+  }
+})();
