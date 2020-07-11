78826c9265dc408afdbfcceaa3309ab0d63736bb
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-07-11 19:00:01 +0100

    test(indiekit): json error response

--- packages/indiekit/tests/middleware/error.js
+++ packages/indiekit/tests/middleware/error.js
@@ -1,3 +1,4 @@
+import HttpError from 'http-errors';
 import test from 'ava';
 import sinon from 'sinon';
 import mockReqRes from 'mock-req-res';
@@ -20,4 +21,35 @@ test('Passes error onto next middleware', async t => {
   const next = sinon.spy();
   await error.notFound(request, response, next);
   t.true(next.calledOnce);
+});
+
+test('Renders error as plain text', async t => {
+  const httpError = new HttpError(400, 'Error messaage', {
+    json: {
+      error: 'bad_request',
+      error_description: 'Error message'
+    }
+  });
+  const request = mockRequest({accepts: () => false});
+  const response = mockResponse();
+  const next = sinon.spy();
+  await error.internalServer(httpError, request, response, next);
+  t.true(response.send.calledWith('Error messaage'));
+});
+
+test('Renders error as JSON', async t => {
+  const httpError = new HttpError(400, 'Error messaage', {
+    json: {
+      error: 'bad_request',
+      error_description: 'Error message'
+    }
+  });
+  const request = mockRequest({accepts: () => true});
+  const response = mockResponse();
+  const next = sinon.spy();
+  await error.internalServer(httpError, request, response, next);
+  t.true(response.json.calledWith({
+    error: 'bad_request',
+    error_description: 'Error message'
+  }));
 });
