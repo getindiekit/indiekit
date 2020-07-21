e1592d9780f0e2557075effd49f3d6e0265728fe
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-07-21 21:11:38 +0100

    refactor: remove custom config

--- indiekit.config.js
+++ indiekit.config.js
@@ -13,45 +13,12 @@ const githubStore = new GithubStore({
   token: process.env.GITHUB_TOKEN
 });
 
+// Register extensions
+indiekit.addStore(githubStore);
+
 // Publication settings
 indiekit.set('publication.me', process.env.PUBLICATION_URL);
 indiekit.set('publication.storeId', 'github');
-indiekit.set('publication.configPresetId', 'jekyll');
-indiekit.set('publication.config.categories', {
-  url: 'https://paulrobertlloyd.com/categories/index.json'
-});
-
-indiekit.set('publication.config.post-types', [{
-  type: 'note',
-  name: 'Note (Config)',
-  // TODO: User configured template locations
-  // template: 'etc/templates/note.njk',
-  post: {
-    path: '_notes/{t}.md',
-    url: 'notes/{t}'
-  }
-}, {
-  type: 'photo',
-  name: 'Photo (Config)',
-  post: {
-    path: '_photos/{t}.md',
-    url: 'photos/{t}'
-  },
-  media: {
-    path: 'src/media/photos/{t}-{filename}',
-    url: 'media/photos/{t}-{filename}'
-  }
-}, {
-  type: 'reply',
-  name: 'Reply (Config)',
-  post: {
-    path: '_replies/{t}.md',
-    url: 'replies/{t}'
-  }
-}]);
-
-// Register extensions
-indiekit.addStore(githubStore);
 
 // Server
 const server = indiekit.server();
