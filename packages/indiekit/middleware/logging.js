a52b4f4c7a7dba15634138d1395c06add4ef7a76
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-08-29 18:24:41 +0100

    fix: update logging

--- packages/indiekit/middleware/logging.js
+++ packages/indiekit/middleware/logging.js
@@ -2,8 +2,13 @@ import Debug from 'debug';
 
 export const logging = (request, response, next) => {
   const debug = new Debug('indiekit:request');
+  debug('url', request.originalUrl);
   debug('headers', request.headers);
-  debug('body', request.body);
+  if (request.is('json')) {
+    debug('body (JSON)', JSON.stringify(request.body));
+  } else {
+    debug('body', request.body);
+  }
 
   next();
 };
