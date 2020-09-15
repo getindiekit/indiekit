7750c320793953f8831d92d795d7a61803563971
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-09-15 21:41:47 +0100

    feat: redirect signed out users to sign in page. fixes #281

--- packages/indiekit/routes/index.js
+++ packages/indiekit/routes/index.js
@@ -1,13 +1,15 @@
 import express from 'express';
 import {assetsPath} from '@indiekit/frontend';
 import {assetsRoutes} from './assets.js';
+import {homepageRoutes} from './homepage.js';
 import {sessionRoutes} from './session.js';
 import {statusRoutes} from './status.js';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 export const routes = () => {
-  router.use('/', statusRoutes);
+  router.use('/', homepageRoutes);
+  router.use('/status', statusRoutes);
   router.use('/assets', assetsRoutes, express.static(assetsPath));
   router.use('/session', sessionRoutes);
 
--- packages/indiekit/tests/routes/homepage.js
+++ packages/indiekit/tests/routes/homepage.js
@@ -5,9 +5,8 @@ import {serverConfig} from '../../config/server.js';
 
 const request = supertest(serverConfig(defaultConfig));
 
-test('Returns status page', async t => {
+test('Logged out users redirected to login page', async t => {
   const response = await request.get('/');
-  t.is(response.status, 200);
-  t.is(response.type, 'text/html');
+  t.is(response.status, 302);
 });
 
