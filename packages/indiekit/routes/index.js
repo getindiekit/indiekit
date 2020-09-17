eb42847b2a7a2eca2ee74b05ef4855c89730149f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-09-17 22:41:00 +0100

    fix: authenticate access to status page

--- packages/indiekit/routes/index.js
+++ packages/indiekit/routes/index.js
@@ -4,12 +4,13 @@ import {assetsRoutes} from './assets.js';
 import {homepageRoutes} from './homepage.js';
 import {sessionRoutes} from './session.js';
 import {statusRoutes} from './status.js';
+import {authenticate} from '../middleware/authentication.js';
 
 const router = express.Router(); // eslint-disable-line new-cap
 
 export const routes = () => {
   router.use('/', homepageRoutes);
-  router.use('/status', statusRoutes);
+  router.use('/status', authenticate, statusRoutes);
   router.use('/assets', assetsRoutes, express.static(assetsPath));
   router.use('/session', sessionRoutes);
 
