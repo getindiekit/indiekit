b583d5807937817d69f5ec3923eb3806b19f6339
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-10-25 01:37:00 +0100

    refactor(indiekit): update logging format

--- packages/indiekit/middleware/logging.js
+++ packages/indiekit/middleware/logging.js
@@ -5,11 +5,7 @@ const debug = new Debug('indiekit:request');
 export const logging = (request, response, next) => {
   debug('url', request.originalUrl);
   debug('headers', request.headers);
-  if (request.is('json')) {
-    debug('body (JSON)', JSON.stringify(request.body));
-  } else {
-    debug('body', request.body);
-  }
+  debug('body', request.body);
 
   next();
 };
