91611ea556bf7d290d5bdbf0c4d9b459462399bf
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-28 13:47:02 +0000

    chore: remove internet archive from server config

--- indiekit.config.js
+++ indiekit.config.js
@@ -2,7 +2,6 @@ import 'dotenv/config.js'; // eslint-disable-line import/no-unassigned-import
 import {Indiekit} from './packages/indiekit/index.js';
 import {JekyllPreset} from './packages/preset-jekyll/index.js';
 import {GithubStore} from './packages/store-github/index.js';
-import {InternetArchiveSyndicator} from './packages/syndicator-internet-archive/index.js';
 
 // New indiekit instance
 const indiekit = new Indiekit();
@@ -18,12 +17,6 @@ const github = new GithubStore({
   token: process.env.GITHUB_TOKEN
 });
 
-// Configure Internet Archive syndicator
-const internetArchive = new InternetArchiveSyndicator({
-  accessKey: process.env.ARCHIVE_ACCESS_KEY,
-  secret: process.env.ARCHIVE_SECRET
-});
-
 // Application settings
 indiekit.set('application.mongodbUrl', process.env.MONGODB_URL);
 
@@ -32,7 +25,6 @@ indiekit.set('publication.me', process.env.PUBLICATION_URL);
 indiekit.set('publication.preset', jekyll);
 indiekit.set('publication.store', github);
 indiekit.set('publication.timeZone', process.env.TZ ? process.env.TZ : 'UTC');
-indiekit.set('publication.syndicationTargets', [internetArchive]);
 
 // Server
 const server = indiekit.server();
