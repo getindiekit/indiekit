9e8129a9d8043476d273b303f719d43281deaf71
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2020-12-31 17:42:11 +0000

    chore: reinstate twitter syndication for testing

--- indiekit.config.js
+++ indiekit.config.js
@@ -2,6 +2,7 @@ import 'dotenv/config.js'; // eslint-disable-line import/no-unassigned-import
 import {Indiekit} from './packages/indiekit/index.js';
 import {JekyllPreset} from './packages/preset-jekyll/index.js';
 import {GithubStore} from './packages/store-github/index.js';
+import {TwitterSyndicator} from './packages/syndicator-twitter/index.js';
 
 // New indiekit instance
 const indiekit = new Indiekit();
@@ -17,6 +18,15 @@ const github = new GithubStore({
   token: process.env.GITHUB_TOKEN
 });
 
+const twitter = new TwitterSyndicator({
+  checked: true,
+  user: process.env.TWITTER_USER,
+  apiKey: process.env.TWITTER_API_KEY,
+  apiKeySecret: process.env.TWITTER_API_KEY_SECRET,
+  accessToken: process.env.TWITTER_ACCESS_TOKEN,
+  accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET
+});
+
 // Application settings
 indiekit.set('application.mongodbUrl', process.env.MONGODB_URL);
 
@@ -24,6 +34,7 @@ indiekit.set('application.mongodbUrl', process.env.MONGODB_URL);
 indiekit.set('publication.me', process.env.PUBLICATION_URL);
 indiekit.set('publication.preset', jekyll);
 indiekit.set('publication.store', github);
+indiekit.set('publication.syndicationTargets', [twitter]);
 indiekit.set('publication.timeZone', process.env.TZ ? process.env.TZ : 'UTC');
 
 // Server
