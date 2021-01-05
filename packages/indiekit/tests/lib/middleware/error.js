ebf425820e7476704e8262f2f693a091b1d63457
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-05 23:39:15 +0000

    test(indiekit): add tests for html error page

--- packages/indiekit/tests/lib/middleware/error.js
+++ packages/indiekit/tests/lib/middleware/error.js
@@ -32,13 +32,15 @@ test('Returns 500 for unknown error', async t => {
   t.true(response.status.calledWith(500));
 });
 
-test('Renders error as plain text', async t => {
-  const httpError = new HttpError(400, 'Error messaage');
-  const request = mockRequest({accepts: () => false});
+test('Renders error as HTML', async t => {
+  const httpError = new HttpError(400, 'Error messaage', {
+    scope: 'test'
+  });
+  const request = mockRequest({accepts: mimeType => mimeType.includes('html')});
   const response = mockResponse();
   const next = sinon.spy();
   await error.internalServer(httpError, request, response, next);
-  t.true(response.send.calledWith('Error messaage'));
+  t.true(response.render.calledWith());
 });
 
 test('Renders error as JSON', async t => {
@@ -50,4 +52,13 @@ test('Renders error as JSON', async t => {
   const next = sinon.spy();
   await error.internalServer(httpError, request, response, next);
   t.true(response.json.calledWith());
+});
+
+test('Renders error as plain text', async t => {
+  const httpError = new HttpError(400, 'Error messaage');
+  const request = mockRequest({accepts: () => false});
+  const response = mockResponse();
+  const next = sinon.spy();
+  await error.internalServer(httpError, request, response, next);
+  t.true(response.send.calledWith('Error messaage'));
 });
