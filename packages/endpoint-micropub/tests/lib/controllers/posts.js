6db6fcd9224c1fdce4ad00b4e5195d0e55675d0a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-05 23:39:38 +0000

    test(endpoint-micropub): add test for 404 page

--- packages/endpoint-micropub/tests/lib/controllers/posts.js
+++ packages/endpoint-micropub/tests/lib/controllers/posts.js
@@ -56,6 +56,17 @@ test('Views previously published post', async t => {
   t.true(response.render.calledWith('post'));
 });
 
+test('Returns 404 if can’t find previously uploaded file', async t => {
+  const request = mockRequest();
+  const response = mockResponse();
+  const next = sinon.spy();
+  await postsController({
+    posts: {findOne: () => false}
+  }).view(request, response, next);
+  t.true(next.calledOnce);
+  t.is(next.firstCall.args[0].message, 'No post was found with this UUID');
+});
+
 test('Throws error to next middleware if can’t view previously published post', async t => {
   const request = mockRequest();
   const response = mockResponse();
