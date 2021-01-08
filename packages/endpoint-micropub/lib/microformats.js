33e248c87fd0fb290eb35d83b80400ab7dc0ab2d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-08 21:26:05 +0000

    feat(endpoint-micropub): convert stored jf2 to mf2 on source query

--- packages/endpoint-micropub/lib/microformats.js
+++ packages/endpoint-micropub/lib/microformats.js
@@ -256,6 +256,36 @@ export const getSyndicateToProperty = (mf2, syndicationTargets) => {
 };
 
 /**
+ * Convert JF2 to Microformats2 object
+ *
+ * @param {string} jf2 JF2
+ * @returns {string} Micropub action
+ */
+export const jf2ToMf2 = jf2 => {
+  const mf2 = {
+    type: [`h-${jf2.type}`],
+    properties: {}
+  };
+
+  delete jf2.type;
+
+  // Convert values to arrays, ie 'a' => ['a'] and move to properties object
+  for (const key in jf2) {
+    if (Object.prototype.hasOwnProperty.call(jf2, key)) {
+      mf2.properties[key] = [].concat(jf2[key]);
+    }
+  }
+
+  // Update key for plaintext content
+  if (mf2.properties.content[0] && mf2.properties.content[0].text) {
+    mf2.properties.content[0].value = jf2.content.text;
+    delete mf2.properties.content[0].text;
+  }
+
+  return mf2;
+};
+
+/**
  * Return microformats of a given URL
  *
  * @param {string} url URL path to post
