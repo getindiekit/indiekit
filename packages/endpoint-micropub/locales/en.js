1cab6ce54425aacb2bd097a89149f8476de8ddf4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-08 21:11:08 +0000

    feat(endpoint-micropub): remove mf2 from post view

--- packages/endpoint-micropub/locales/en.js
+++ packages/endpoint-micropub/locales/en.js
@@ -1,7 +1,6 @@
 export const en = {
   micropub: {
     title: 'Posts',
-    microformats: 'Microformats for this post',
     properties: 'Properties',
     posts: {
       title: 'Published posts'
--- packages/endpoint-micropub/locales/fr.js
+++ packages/endpoint-micropub/locales/fr.js
@@ -1,7 +1,6 @@
 export const fr = {
   micropub: {
     title: 'Articles',
-    microformats: 'Microformats pour cet article',
     properties: 'Propriétés',
     posts: {
       title: 'Article publié'
