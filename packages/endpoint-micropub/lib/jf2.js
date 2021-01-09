18d7f1646bf6dff646fd495044e6a645319fe21f
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-09 14:43:13 +0000

    refactor(endpoint-micropub): split jf2 and mf2 functions

--- packages/endpoint-micropub/lib/jf2.js
+++ packages/endpoint-micropub/lib/jf2.js
@@ -1,8 +1,6 @@
-import got from 'got';
-import parser from 'microformats-parser';
+import {getDate} from './date.js';
 import {markdownToHtml, htmlToMarkdown} from './markdown.js';
 import {reservedProperties} from './reserved-properties.js';
-import {getDate} from './date.js';
 import {
   decodeQueryParameter,
   excerptString,
@@ -78,44 +76,6 @@ export const getJf2 = (publication, properties) => {
   }
 
   return properties;
-};
-
-/**
- * Returns/selects microformat properties of a post.
- *
- * @param {object} mf2 Microformats2 object
- * @param {Array|string} requestedProperties mf2 properties to select
- * @returns {Promise|object} mf2 with requested properties
- */
-export const getMf2Properties = (mf2, requestedProperties) => {
-  const mf2HasItems = mf2.items && mf2.items.length > 0;
-  if (!mf2HasItems) {
-    throw new Error('Source has no items');
-  }
-
-  const item = mf2.items[0];
-  const {properties} = item;
-
-  // Return requested properties
-  if (requestedProperties) {
-    const selectedProperties = {};
-
-    if (!Array.isArray(requestedProperties)) {
-      requestedProperties = new Array(requestedProperties);
-    }
-
-    requestedProperties.forEach(key => {
-      if (properties[key]) {
-        selectedProperties[key] = properties[key];
-      }
-    });
-
-    item.properties = selectedProperties;
-  }
-
-  // Return properties
-  delete item.type;
-  return item;
 };
 
 /**
@@ -244,49 +204,4 @@ export const getSyndicateToProperty = (properties, syndicationTargets) => {
   if (syndication.length > 0) {
     return syndication;
   }
-};
-
-/**
- * Convert JF2 to Microformats2 object
- *
- * @param {string} jf2 JF2
- * @returns {string} Micropub action
- */
-export const jf2ToMf2 = jf2 => {
-  const mf2 = {
-    type: [`h-${jf2.type}`],
-    properties: {}
-  };
-
-  delete jf2.type;
-
-  // Convert values to arrays, ie 'a' => ['a'] and move to properties object
-  for (const key in jf2) {
-    if (Object.prototype.hasOwnProperty.call(jf2, key)) {
-      mf2.properties[key] = [].concat(jf2[key]);
-    }
-  }
-
-  // Update key for plaintext content
-  if (mf2.properties.content[0] && mf2.properties.content[0].text) {
-    mf2.properties.content[0].value = jf2.content.text;
-    delete mf2.properties.content[0].text;
-  }
-
-  return mf2;
-};
-
-/**
- * Return microformats of a given URL
- *
- * @param {string} url URL path to post
- * @returns {Promise|object} Microformats2 object
- */
-export const url2Mf2 = async url => {
-  const {body} = await got(url);
-  const mf2 = parser.mf2(body, {
-    baseUrl: url
-  });
-
-  return mf2;
 };
--- packages/endpoint-micropub/tests/lib/jf2.js
+++ packages/endpoint-micropub/tests/lib/jf2.js
@@ -1,22 +1,17 @@
 import test from 'ava';
-import nock from 'nock';
 import dateFns from 'date-fns';
-import parser from 'microformats-parser';
 import {getFixture} from '../helpers/fixture.js';
 import {
   formEncodedToJf2,
   getJf2,
-  getMf2Properties,
   getAudioProperty,
   getContentProperty,
   getPhotoProperty,
   getVideoProperty,
   getPublishedProperty,
   getSlugProperty,
-  getSyndicateToProperty,
-  jf2ToMf2,
-  url2Mf2
-} from '../../lib/microformats.js';
+  getSyndicateToProperty
+} from '../../lib/jf2.js';
 
 const {isValid, parseISO} = dateFns;
 
@@ -29,9 +24,7 @@ test.beforeEach(t => {
         uid: 'https://social.example/'
       }],
       timeZone: 'UTC'
-    },
-    nock: nock('https://website.example').get('/post.html'),
-    url: 'https://website.example/post.html'
+    }
   };
 });
 
@@ -88,53 +81,6 @@ test('Gets JF2 object (all properties)', t => {
   t.deepEqual(result.category, ['lunch', 'food']);
   t.true(isValid(parseISO(result.published)));
   t.deepEqual(result['mp-syndicate-to'], ['https://social.example/']);
-});
-
-test('Returns mf2 item with all properties', t => {
-  const mf2 = parser.mf2(getFixture('post.html'), {baseUrl: t.context.url});
-  const result = getMf2Properties(mf2);
-  t.deepEqual(result, {
-    properties: {
-      name: ['I ate a cheese sandwich, which was nice.'],
-      published: ['2013-03-07'],
-      content: ['I ate a cheese sandwich, which was nice.']
-    }
-  });
-});
-
-test('Returns mf2 item with multiple properties', t => {
-  const mf2 = parser.mf2(getFixture('post.html'), {baseUrl: t.context.url});
-  const result = getMf2Properties(mf2, ['name', 'published']);
-  t.deepEqual(result, {
-    properties: {
-      name: ['I ate a cheese sandwich, which was nice.'],
-      published: ['2013-03-07']
-    }
-  });
-});
-
-test('Returns mf2 item with single property', t => {
-  const mf2 = parser.mf2(getFixture('post.html'), {baseUrl: t.context.url});
-  const result = getMf2Properties(mf2, 'name');
-  t.deepEqual(result, {
-    properties: {
-      name: ['I ate a cheese sandwich, which was nice.']
-    }
-  });
-});
-
-test('Returns mf2 item with empty object if property not found', t => {
-  const mf2 = parser.mf2(getFixture('post.html'), {baseUrl: t.context.url});
-  const result = getMf2Properties(mf2, 'location');
-  t.deepEqual(result, {
-    properties: {}
-  });
-});
-
-test('Throws error if mf2 has no items', t => {
-  const mf2 = parser.mf2(getFixture('page.html'), {baseUrl: t.context.url});
-  const error = t.throws(() => getMf2Properties(mf2, 'name'));
-  t.is(error.message, 'Source has no items');
 });
 
 test('Gets audio property', t => {
@@ -359,82 +305,4 @@ test('Doesn’t add unavailable syndication target', t => {
   const syndicationTargets = [];
   const result = getSyndicateToProperty(properties, syndicationTargets);
   t.falsy(result);
-});
-
-test('Converts JF2 to Microformats2 object', async t => {
-  const feed = JSON.parse(getFixture('feed.jf2'));
-  const result = await jf2ToMf2(feed.children[0]);
-  t.deepEqual(result, {
-    type: ['h-entry'],
-    properties: {
-      uid: ['https://website.example/second-item'],
-      url: ['https://website.example/second-item'],
-      name: ['Second item in feed'],
-      content: [{
-        value: 'This second item has all fields.',
-        html: '<p>This second item has <strong>all</strong> fields.</p>'
-      }],
-      summary: ['This is the second item'],
-      featured: ['https://another.example/banner_image.jpg'],
-      published: ['2020-12-31T17:05:55+00:00'],
-      updated: ['2021-01-01T12:05:55+00:00'],
-      author: [{
-        name: 'Joe Bloggs',
-        url: 'https://website.example/~joebloggs',
-        photo: 'https://website.example/~joebloggs/photo.jpg'
-      }],
-      category: ['second', 'example'],
-      audio: [{
-        url: 'https://website.example/second-item/audio.weba',
-        alt: 'Audio',
-        'content-type': 'audio/webm'
-      }],
-      photo: [{
-        url: 'https://website.example/second-item/photo.webp',
-        alt: 'Photo',
-        'content-type': 'image/webp'
-      }],
-      video: [{
-        url: 'https://website.example/second-item/audio.webm',
-        alt: 'Video',
-        'content-type': 'video/webm'
-      }]
-    }
-  });
-});
-
-test('Returns mf2 from URL', async t => {
-  const scope = t.context.nock.reply(200, getFixture('post.html'));
-  const result = await url2Mf2(t.context.url);
-  t.deepEqual(result, {
-    rels: {},
-    'rel-urls': {},
-    items: [{
-      type: ['h-entry'],
-      properties: {
-        name: ['I ate a cheese sandwich, which was nice.'],
-        published: ['2013-03-07'],
-        content: ['I ate a cheese sandwich, which was nice.']
-      }
-    }]
-  });
-  scope.done();
-});
-
-test('Returns mf2 empty objects if no microformats found', async t => {
-  const scope = t.context.nock.reply(200, getFixture('page.html'));
-  const result = await url2Mf2(t.context.url);
-  t.deepEqual(result, {
-    rels: {},
-    'rel-urls': {},
-    items: []
-  });
-  scope.done();
-});
-
-test('Throws error if URL not found', async t => {
-  const scope = t.context.nock.replyWithError('Not found');
-  const error = await t.throwsAsync(url2Mf2(t.context.url));
-  t.is(error.message, 'Not found');
-  scope.done();
 });
