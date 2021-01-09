9a61bddade82bcbb8267beda27114ee3fb99c9ad
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-01-09 01:29:32 +0000

    refactor(endpoint-syndicate): remove jf2 to mf2 conversion

--- packages/endpoint-syndicate/tests/fixtures/posts-syndicated.js
+++ packages/endpoint-syndicate/tests/fixtures/posts-syndicated.js
@@ -9,14 +9,6 @@ export const publication = {
         syndication: 'https://social.example/status/1',
         'post-type': 'note',
         url: 'https://website.example/syndicated-post'
-      },
-      mf2: {
-        type: ['h-entry'],
-        properties: {
-          content: ['hello world'],
-          published: ['2019-08-17T23:56:38.977+01:00'],
-          syndication: ['https://social.example/status/1']
-        }
       }
     })
   },
--- packages/endpoint-syndicate/tests/fixtures/posts.js
+++ packages/endpoint-syndicate/tests/fixtures/posts.js
@@ -14,16 +14,6 @@ export const posts = {
       url: 'https://paulrobertlloyd.github.io/indiekit-sandbox/notes/2020/10/17/12345',
       'mp-slug': '12345',
       'mp-syndicate-to': 'https://social.example/'
-    },
-    mf2: {
-      type: ['h-entry'],
-      properties: {
-        name: ['Item in database'],
-        published: ['2020-10-17T19:41:39Z'],
-        url: ['https://paulrobertlloyd.github.io/indiekit-sandbox/notes/2020/10/17/12345'],
-        'mp-slug': ['12345'],
-        'mp-syndicate-to': ['https://social.example/']
-      }
     }
   }),
   replaceOne: async () => {}
