3b4a26b8a2f84d31adf284a7aeb2f718e3351bf8
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-02-07 16:59:50 +0000

    test(indiekit): refactor cache tests

--- packages/indiekit/tests/unit/cache.js
+++ packages/indiekit/tests/unit/cache.js
@@ -1,21 +1,15 @@
 import test from 'ava';
 import nock from 'nock';
-import {mongodbConfig} from '../../config/mongodb.js';
+import {indiekitConfig} from '@indiekit-test/config';
 import {Cache} from '../../lib/cache.js';
 
 test.beforeEach(async t => {
-  const database = await mongodbConfig(process.env.TEST_MONGODB_URL);
-  const collection = await database.collection('cache');
+  const {application} = await indiekitConfig;
 
   t.context = {
-    cache: new Cache(collection),
+    cache: new Cache(application.cache),
     url: 'https://website.example/categories.json'
   };
-});
-
-test.afterEach.always(async () => {
-  const database = await mongodbConfig(process.env.TEST_MONGODB_URL);
-  await database.dropDatabase();
 });
 
 test.serial('Returns data from remote file and saves to cache', async t => {
