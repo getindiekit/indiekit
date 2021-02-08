19cc092f1d4a432d29d4b833749d76079628b24c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2021-02-08 22:12:05 +0000

    test(indiekit): refactor cache tests

--- packages/indiekit/tests/unit/cache.js
+++ packages/indiekit/tests/unit/cache.js
@@ -7,41 +7,46 @@ test.beforeEach(async t => {
   const {application} = await indiekitConfig;
 
   t.context = {
-    cache: new Cache(application.cache),
-    url: 'https://website.example/categories.json'
+    cacheCollection: application.cache,
+    url: `${process.env.TEST_PUBLICATION_URL}categories.json`
   };
 });
 
-test.serial('Returns data from remote file and saves to cache', async t => {
-  nock('https://website.example')
+test('Returns data from remote file and saves to cache', async t => {
+  nock(process.env.TEST_PUBLICATION_URL)
     .get('/categories.json')
     .reply(200, ['Foo', 'Bar']);
+  const cache = new Cache(t.context.cacheCollection);
 
-  const result = await t.context.cache.json('category', t.context.url);
+  const result = await cache.json('test1', t.context.url);
 
   t.is(result.source, t.context.url);
 });
 
-test.serial('Throws error if remote file not found', async t => {
-  nock('https://website.example')
+test('Throws error if remote file not found', async t => {
+  nock(process.env.TEST_PUBLICATION_URL)
     .get('/categories.json')
     .replyWithError('Not found');
+  const cache = new Cache(t.context.cacheCollection);
 
-  await t.throwsAsync(t.context.cache.json('file', t.context.url), {
+  await t.throwsAsync(cache.json('test2', t.context.url), {
     message: `Unable to fetch ${t.context.url}: Not found`
   });
 });
 
-test.serial('Gets data from cache', async t => {
-  const cache = new Cache({
-    findOne: async () => ({
-      souce: 'cache',
-      data: {}
-    })
+test('Gets data from cache', async t => {
+  t.context.cacheCollection.insertOne({
+    key: 'test3',
+    url: t.context.url,
+    data: ['Foo', 'Bar']
   });
-  await cache.json('file', t.context.url);
+  const cache = new Cache(t.context.cacheCollection);
 
-  const result = await cache.json('file', t.context.url);
+  // Request item and add to cache
+  await cache.json('test3', t.context.url);
+
+  // Retrieve item from cache
+  const result = await cache.json('test3', t.context.url);
 
   t.is(result.source, 'cache');
 });
