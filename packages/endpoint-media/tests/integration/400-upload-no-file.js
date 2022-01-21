7b45f7c163c169776a2e991f9ede502e99ab51cc
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-01-21 16:33:00 +0000

    test(endpoint-media): remove serial test

--- packages/endpoint-media/tests/integration/400-upload-no-file.js
+++ packages/endpoint-media/tests/integration/400-upload-no-file.js
@@ -3,7 +3,7 @@ import test from 'ava';
 import nock from 'nock';
 import {server} from '@indiekit-test/server';
 
-test.serial('Returns 400 if no file included in request', async t => {
+test('Returns 400 if no file included in request', async t => {
   nock('https://tokens.indieauth.com')
     .get('/token')
     .reply(200, {
