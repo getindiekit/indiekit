0fe768ee0fd79421bb05ad3edae0c1931061abb0
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-01-23 16:01:20 +0000

    fix: improve appearance of post and syndication target icons

--- packages/frontend/tests/unit/nunjucks/globals.js
+++ packages/frontend/tests/unit/nunjucks/globals.js
@@ -5,5 +5,5 @@ const {icon} = require('../../../lib/nunjucks/globals.js');
 test('Renders SVG icon', t => {
   const result = icon('note');
 
-  t.regex(result, /^<svg xmlns="http:\/\/www.w3.org\/2000\/svg"/);
+  t.regex(result, /^<svg class="icon" xmlns="http:\/\/www.w3.org\/2000\/svg"/);
 });
