5fbec3bfdccfea4868cac197e5eb434fa22b87b4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-04-10 23:26:35 +0100

    test(indiekit): force https

--- packages/indiekit/tests/unit/middleware/force-https.js
+++ packages/indiekit/tests/unit/middleware/force-https.js
@@ -20,3 +20,21 @@ test("Redirect HTTP requests to HTTPS", async (t) => {
 
   t.true(response.redirect.calledWith(302));
 });
+
+test("Marks proxy as secure", async (t) => {
+  const request = mockRequest({
+    connection: {},
+    headers: {
+      "x-forwarded-proto": "https",
+      Host: "server.example",
+    },
+    originalUrl: "/foo?bar=qux",
+  });
+  const response = mockResponse();
+  const next = sinon.spy();
+
+  await forceHttps(request, response, next);
+
+  t.true(next.calledOnce);
+  t.true(request.connection.proxySecure);
+});
