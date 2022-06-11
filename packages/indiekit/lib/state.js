c336829ca81f48c756ae133219f992984746a377
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-06-12 00:45:15 +0100

    refactor(indiekit): move state functions into seperate file

---
+++ packages/indiekit/lib/state.js
@@ -0,0 +1,44 @@
+import { decrypt, encrypt } from "./utils.js";
+
+/**
+ * Generate unique encrypted state value
+ *
+ * @param {string} clientId Client ID
+ * @param {object} iv Initialization vector
+ * @returns {string} State
+ */
+export const generateState = (clientId, iv) => {
+  console.log(typeof iv);
+
+  const state = encrypt(
+    JSON.stringify({
+      clientId,
+      date: Date.now(),
+    }),
+    iv
+  );
+
+  return state;
+};
+
+/**
+ * Validate state generated using `generateState` method
+ *
+ * @param {string} state State
+ * @param {string} clientId Client ID
+ * @param {object} iv Initialization vector
+ * @returns {object|boolean} Validated state object, returns false on failure
+ */
+export const validateState = (state, clientId, iv) => {
+  try {
+    state = JSON.parse(decrypt(state, iv));
+    const validClient = state.clientId === clientId;
+    const validDate = state.date > Date.now() - 1000 * 60 * 10;
+
+    if (validClient && validDate) {
+      return state;
+    }
+  } catch {
+    return false;
+  }
+};
