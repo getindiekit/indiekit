b9d05da4ef28856546e71062e85161b620958604
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-06-17 22:13:30 +0100

    feat(frontend): use stimulus to enhance components

---
+++ packages/frontend/components/preview/index.js
@@ -0,0 +1,39 @@
+import { Controller } from "@hotwired/stimulus";
+
+export const PreviewController = class extends Controller {
+  static targets = ["text", "title", "url"];
+
+  connect() {
+    this.updatePreview(this.textTarget);
+    this.updatePreview(this.titleTarget);
+    this.updatePreview(this.urlTarget);
+  }
+
+  text(event) {
+    this.updatePreview(this.textTarget, event.target);
+  }
+
+  title(event) {
+    this.updatePreview(this.titleTarget, event.target);
+  }
+
+  url(event) {
+    this.updatePreview(this.urlTarget, event.target);
+  }
+
+  /**
+   * Update preview in output based on inputted value
+   *
+   * @param {HTMLElement} outputElement - Output element
+   * @param {HTMLElement} inputElement - Input element
+   */
+  updatePreview(outputElement, inputElement = false) {
+    outputElement.classList.add("placeholder");
+    outputElement.value = outputElement.dataset.placeholder;
+
+    if (inputElement && inputElement.value !== "") {
+      outputElement.classList.remove("placeholder");
+      outputElement.value = decodeURIComponent(inputElement.value);
+    }
+  }
+};
--- packages/frontend/index.js
+++ packages/frontend/index.js
@@ -3,3 +3,4 @@ import { fileURLToPath } from "node:url";
 export const assetsPath = fileURLToPath(new URL("assets", import.meta.url));
 export { templates } from "./lib/nunjucks.js";
 export { styles } from "./lib/postcss.js";
+export { scripts } from "./lib/rollup.js";
