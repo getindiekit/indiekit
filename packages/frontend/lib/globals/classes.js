b1efbae0896348a542f1de8ea8d6932619a41414
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-17 13:13:02 +0000

    feat(frontend): smarter classes global

--- packages/frontend/lib/globals/classes.js
+++ packages/frontend/lib/globals/classes.js
@@ -2,11 +2,19 @@
  * Generate space-separated list of class names
  *
  * @param {string} baseClass - Base class name
- * @param {string} [optionalClasses] - List of space-separated class names
+ * @param {string} [options={}] - Component options
  * @returns {string} Space-separated list of class names
  */
-export const classes = (baseClass, optionalClasses) => {
-  return optionalClasses
-    ? [baseClass, ...optionalClasses.split(" ")].join(" ")
-    : baseClass;
+export const classes = (baseClass, options = {}) => {
+  const classes = [baseClass];
+
+  if (options.errorMessage) {
+    classes.push(`${baseClass}--error`);
+  }
+
+  if (options.classes) {
+    classes.push(options.classes);
+  }
+
+  return classes.join(" ");
 };
--- packages/frontend/tests/unit/globals/classes.js
+++ packages/frontend/tests/unit/globals/classes.js
@@ -2,9 +2,20 @@ import test from "ava";
 import { classes } from "../../../lib/globals/index.js";
 
 test("Generates space-separated list of class names", (t) => {
-  const result1 = classes("foo");
-  const result2 = classes("foo", "bar baz");
+  const result = classes("foo");
+  const resultWithClasses = classes("foo", {
+    classes: "bar baz",
+  });
+  const resultWithError = classes("foo", {
+    errorMessage: "Enter a qux",
+  });
+  const resultWithErrorAndClasses = classes("foo", {
+    classes: "bar baz",
+    errorMessage: "Enter a qux",
+  });
 
-  t.is(result1, "foo");
-  t.is(result2, "foo bar baz");
+  t.is(result, "foo");
+  t.is(resultWithClasses, "foo bar baz");
+  t.is(resultWithError, "foo foo--error");
+  t.is(resultWithErrorAndClasses, "foo foo--error bar baz");
 });
