a9f6da588f7899c839c6ba30e0fdde6e6bc98010
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-21 13:24:10 +0000

    feat(frontend): smarter summaryRows global

--- packages/frontend/lib/globals/summary-rows.js
+++ packages/frontend/lib/globals/summary-rows.js
@@ -10,15 +10,24 @@ export const summaryRows = (object) => {
   const rows = [];
 
   for (const [key, value] of Object.entries(object)) {
+    const valueText = (value) => {
+      if (typeof value === "string" || typeof value === "boolean") {
+        return `<code class="token attr-value">${value}</code>`;
+      }
+
+      try {
+        return markdown(`~~~js\n${JSON.stringify(value, 0, 2)}\n~~~`);
+      } catch {
+        return `<code class="token class-name">${value.constructor.name}</code>`;
+      }
+    };
+
     rows.push({
       key: {
         text: key,
       },
       value: {
-        text:
-          typeof value === "string"
-            ? value
-            : markdown(`~~~json\n${JSON.stringify(value, 0, 2)}\n~~~`),
+        text: valueText(value),
       },
     });
   }
--- packages/frontend/tests/unit/globals/summary-row.js
+++ packages/frontend/tests/unit/globals/summary-row.js
@@ -4,8 +4,10 @@ import { summaryRows } from "../../../lib/globals/index.js";
 test("Transforms object into array for consumption by summary component", (t) => {
   const result = summaryRows({
     me: "https://website.example",
+    categories: ["foo", "bar"],
   });
 
   t.is(result[0].key.text, "me");
-  t.is(result[0].value.text, "https://website.example");
+  t.regex(result[0].value.text, /^<code class="token attr-value">/);
+  t.regex(result[1].value.text, /^<pre class="language-js">/);
 });
