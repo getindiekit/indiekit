0a9573685676817d5281c79d2e68a1a8a92fbb5e
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-23 23:08:10 +0000

    refactor(frontend): remove postcss extends plugin

--- packages/frontend/lib/postcss.js
+++ packages/frontend/lib/postcss.js
@@ -3,12 +3,11 @@ import { fileURLToPath } from "node:url";
 import postcss from "postcss";
 import postcssCustomMedia from "postcss-custom-media";
 import postcssEasyImport from "postcss-easy-import";
-import postcssExtendRule from "postcss-extend-rule";
 
 export const styles = async () => {
   const options = {
     from: fileURLToPath(new URL("../styles/app.css", import.meta.url)),
-    plugins: [postcssEasyImport, postcssCustomMedia, postcssExtendRule],
+    plugins: [postcssEasyImport, postcssCustomMedia],
   };
 
   const css = fs.readFileSync(options.from, "utf8");
