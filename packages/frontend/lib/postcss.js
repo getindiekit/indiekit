7f017d682dd8ae96475c44efb0ef002bd4c56d4a
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-23 23:59:53 +0000

    refactor(frontend): use lightning css for transforming styles

--- packages/frontend/lib/postcss.js
+++ packages/frontend/lib/postcss.js
@@ -1,13 +1,24 @@
 import fs from "node:fs";
 import { fileURLToPath } from "node:url";
 import postcss from "postcss";
-import postcssCustomMedia from "postcss-custom-media";
 import postcssEasyImport from "postcss-easy-import";
+import postcssLightningcss from "postcss-lightningcss";
 
 export const styles = async () => {
   const options = {
     from: fileURLToPath(new URL("../styles/app.css", import.meta.url)),
-    plugins: [postcssEasyImport, postcssCustomMedia],
+    plugins: [
+      postcssEasyImport,
+      postcssLightningcss({
+        browsers: ">= 0.5%",
+        lightningcssOptions: {
+          drafts: {
+            customMedia: true,
+            nesting: true,
+          },
+        },
+      }),
+    ],
   };
 
   const css = fs.readFileSync(options.from, "utf8");
