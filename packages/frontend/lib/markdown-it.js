f81b6ecda28272c7c57066b4ef79870772a65a99
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-11-24 20:11:50 +0000

    refactor(frontend): remove unused markdown-it plugins

--- packages/frontend/lib/markdown-it.js
+++ packages/frontend/lib/markdown-it.js
@@ -1,6 +1,4 @@
 import markdownIt from "markdown-it";
-import markdownItAnchor from "markdown-it-anchor";
-import markdownItDeflist from "markdown-it-deflist";
 import markdownItPrism from "markdown-it-prism";
 
 export default (() => {
@@ -12,12 +10,7 @@ export default (() => {
   };
 
   const parser = markdownIt(options);
-  parser
-    .use(markdownItAnchor, {
-      permalink: markdownItAnchor.permalink.headerLink(),
-    })
-    .use(markdownItDeflist)
-    .use(markdownItPrism);
+  parser.use(markdownItPrism);
 
   return parser;
 })();
