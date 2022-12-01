768f444a0e03cf44bc2f6105fa10f9e96ed4c8ad
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-12-01 08:35:11 +0000

    test(indiekit): remove unused nock

--- packages/indiekit/tests/integration/302-session-login-auth.js
+++ packages/indiekit/tests/integration/302-session-login-auth.js
@@ -1,15 +1,8 @@
-import process from "node:process";
 import test from "ava";
-import nock from "nock";
 import supertest from "supertest";
-import { getFixture } from "@indiekit-test/fixtures";
 import { testServer } from "@indiekit-test/server";
 
 test("Login redirects to IndieAuth authentication URL", async (t) => {
-  nock(process.env.TEST_PUBLICATION_URL)
-    .get("/")
-    .reply(200, getFixture("html/home.html"));
-
   const server = await testServer();
   const request = supertest.agent(server);
   const result = await request.post("/session/login");
