7798d72f992a7a6e95c9638189c19b54fce0cbfa
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2022-12-01 08:34:39 +0000

    test(endpoint-micropub): remove unused nock

--- packages/endpoint-micropub/tests/integration/403-action-unauthorized-action.js
+++ packages/endpoint-micropub/tests/integration/403-action-unauthorized-action.js
@@ -1,14 +1,9 @@
 import process from "node:process";
 import test from "ava";
-import nock from "nock";
 import supertest from "supertest";
 import { testServer } from "@indiekit-test/server";
 
 test("Returns 403 error action not supported (by scope)", async (t) => {
-  nock("https://api.github.com")
-    .put((uri) => uri.includes("foobar.md"))
-    .reply(200);
-
   // Create post
   const server = await testServer();
   const request = supertest.agent(server);
