675f945eea9ec638318db3ab39e21dc4b7600fbe
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-07 19:57:13 +0000

    feat(frontend): widont filter

--- packages/frontend/lib/filters/index.js
+++ packages/frontend/lib/filters/index.js
@@ -1,4 +1,5 @@
 export { default as slugify } from "@sindresorhus/slugify";
+export { default as widont } from "widont";
 export { date, languageName, languageNativeName } from "./locale.js";
 export { markdown } from "./markdown.js";
 export { absoluteUrl, friendlyUrl } from "./url.js";
