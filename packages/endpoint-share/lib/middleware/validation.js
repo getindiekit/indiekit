c128a3e416dd636f7600d50e948d59d4fa489a2b
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-08 19:34:08 +0000

    refactor(endpoint-share): reorganise locale catalog strings

--- packages/endpoint-share/lib/middleware/validation.js
+++ packages/endpoint-share/lib/middleware/validation.js
@@ -3,9 +3,9 @@ import { check } from "express-validator";
 export const validate = [
   check("name")
     .notEmpty()
-    .withMessage((value, { req, path }) => req.__(`share.error.${path}`)),
+    .withMessage((value, { req, path }) => req.__(`share.error.${path}.empty`)),
   check("bookmark-of")
     .exists()
     .isURL()
-    .withMessage((value, { req, path }) => req.__(`share.error.${path}`)),
+    .withMessage((value, { req, path }) => req.__(`share.error.${path}.empty`)),
 ];
