62da2ed2621b12a5c1f9c6485321fd01b906dcf5
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-12 01:59:45 +0000

    feat(endpoint-posts): create/update reply post

--- packages/endpoint-posts/lib/middleware/validation.js
+++ packages/endpoint-posts/lib/middleware/validation.js
@@ -6,6 +6,11 @@ export const validate = [
     .exists()
     .isURL()
     .withMessage((value, { req, path }) => req.__(`posts.error.${path}.empty`)),
+  check("in-reply-to")
+    .if((value, { req }) => req.body?.["post-type"] === "reply")
+    .exists()
+    .isURL()
+    .withMessage((value, { req, path }) => req.__(`posts.error.${path}.empty`)),
   check("name")
     .if(
       (value, { req }) =>
@@ -18,7 +23,8 @@ export const validate = [
     .if(
       (value, { req }) =>
         req.body?.["post-type"] === "article" ||
-        req.body?.["post-type"] === "note"
+        req.body?.["post-type"] === "note" ||
+        req.body?.["post-type"] === "reply"
     )
     .notEmpty()
     .withMessage((value, { req, path }) => req.__(`posts.error.${path}.empty`)),
