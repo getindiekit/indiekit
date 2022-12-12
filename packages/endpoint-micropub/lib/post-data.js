118c0e3d904e6fa5e79d2c7de5a74f383e1aebdc
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-12 16:03:48 +0000

    feat(endpoint-micropub): refactor delete/undelete

--- packages/endpoint-micropub/lib/post-data.js
+++ packages/endpoint-micropub/lib/post-data.js
@@ -1,5 +1,6 @@
 import { IndiekitError } from "@indiekit/error";
 import { getPostType } from "./post-type-discovery.js";
+import { getDate } from "./date.js";
 import { getSyndicateToProperty, normaliseProperties } from "./jf2.js";
 import * as update from "./update.js";
 import { getPermalink, getPostTypeConfig, renderPath } from "./utils.js";
@@ -10,9 +11,10 @@ export const postData = {
    *
    * @param {object} publication - Publication configuration
    * @param {object} properties - JF2 properties
+   * @param {boolean} [draftMode=false] - Draft mode
    * @returns {object} Post data
    */
-  async create(publication, properties) {
+  async create(publication, properties, draftMode = false) {
     const { me, postTypes, syndicationTargets } = publication;
 
     // Add syndication targets
@@ -23,6 +25,9 @@ export const postData = {
 
     // Normalise properties
     properties = normaliseProperties(publication, properties);
+
+    // Add published date (or use that provided by client)
+    properties.published = getDate(publication.timeZone, properties.published);
 
     // Post type
     const type = getPostType(properties);
@@ -42,6 +47,12 @@ export const postData = {
     );
     const url = await renderPath(typeConfig.post.url, properties, publication);
     properties.url = getPermalink(me, url);
+
+    // Post status
+    // Draft mode: Only create post with a `draft` post-status
+    properties["post-status"] = draftMode
+      ? "draft"
+      : properties["post-status"] || "published";
 
     // Post data
     const postData = { path, properties };
@@ -70,6 +81,7 @@ export const postData = {
 
   /**
    * Update post data
+   * Add, delete or replace properties and/or replace property values
    *
    * @param {object} publication - Publication configuration
    * @param {string} url - URL of existing post
@@ -102,6 +114,9 @@ export const postData = {
     // Normalise properties
     properties = normaliseProperties(publication, properties);
 
+    // Add updated date
+    properties.updated = getDate(publication.timeZone);
+
     // Post type
     const type = getPostType(properties);
     const typeConfig = getPostTypeConfig(type, postTypes);
@@ -120,8 +135,89 @@ export const postData = {
     );
     properties.url = getPermalink(me, updatedUrl);
 
+    // Return updated post data
+    const postData = { path, properties };
+    return postData;
+  },
+
+  /**
+   * Delete post data
+   * Delete (most) properties, keeping a record of deleted for later retrieval
+   *
+   * @param {object} publication - Publication configuration
+   * @param {string} url - URL of existing post
+   * @returns {object} Post data
+   */
+  async delete(publication, url) {
+    const { postTypes } = publication;
+
+    // Read properties
+    const { properties } = await this.read(publication, url);
+
+    // Make a copy of existing properties
+    const _deletedProperties = structuredClone(properties);
+
+    // Delete all properties, except those required for path creation
+    for (const key in _deletedProperties) {
+      if (!["mp-slug", "post-type", "published", "type", "url"].includes(key)) {
+        delete properties[key];
+      }
+    }
+
+    // Add deleted date
+    properties.deleted = getDate(publication.timeZone);
+
+    // Post type
+    const typeConfig = getPostTypeConfig(properties["post-type"], postTypes);
+
+    // Post paths
+    const path = await renderPath(
+      typeConfig.post.path,
+      properties,
+      publication
+    );
+
     // Return post data
-    const updatedPostData = { path, properties };
-    return updatedPostData;
+    const postData = { path, properties, _deletedProperties };
+    return postData;
+  },
+
+  /**
+   * Undelete post data
+   * Restore previously deleted properties
+   *
+   * @param {object} publication - Publication configuration
+   * @param {string} url - URL of existing post
+   * @param {boolean} [draftMode=false] - Draft mode
+   * @returns {object} Post data
+   */
+  async undelete(publication, url, draftMode) {
+    const { postTypes } = publication;
+
+    // Read deleted properties
+    const { _deletedProperties } = await this.read(publication, url);
+
+    // Restore previously deleted properties
+    const properties = _deletedProperties;
+
+    // Post type
+    const typeConfig = getPostTypeConfig(properties["post-type"], postTypes);
+
+    // Post paths
+    const path = await renderPath(
+      typeConfig.post.path,
+      properties,
+      publication
+    );
+
+    // Post status
+    // Draft mode: Only restore post with a `draft` post-status
+    properties["post-status"] = draftMode
+      ? "draft"
+      : properties["post-status"] || "published";
+
+    // Return post data
+    const postData = { path, properties, _deletedProperties };
+    return postData;
   },
 };
