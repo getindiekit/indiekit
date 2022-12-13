f144f4de5eda56e6506fde7416edb7961c8ecaab
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-13 23:08:08 +0000

    test(media-data): correct properties

--- helpers/media-data/index.js
+++ helpers/media-data/index.js
@@ -1,10 +1,7 @@
 export const mediaData = {
   path: "photo.jpg",
   properties: {
-    filename: "photo.jpg",
-    fileext: "jpg",
-    originalname: "photo.jpg",
-    "media-type": "image/jpeg",
+    "content-type": "image/jpeg",
     "post-type": "photo",
     published: "2020-01-01T00:00:00+00:00",
     url: "https://website.example/photo.jpg",
