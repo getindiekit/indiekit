0e4dff15137f83a7a06e59a1315e1bc0f5480f5c
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-13 02:24:01 +0000

    feat(frontend): review accessibility and design of notification component

---
+++ packages/frontend/components/notification/index.js
@@ -0,0 +1,24 @@
+import { Controller } from "@hotwired/stimulus";
+
+export const NotificationController = class extends Controller {
+  initialize() {
+    const { element } = this;
+
+    if (element.getAttribute("role") !== "alert") {
+      return;
+    }
+
+    // Set tabindex to -1 to make element focusable with JavaScript.
+    // Remove tabindex on blur as component doesn’t need to be focusable
+    // after page has loaded.
+    if (!element.getAttribute("tabindex")) {
+      element.setAttribute("tabindex", "-1");
+
+      element.addEventListener("blur", function () {
+        element.removeAttribute("tabindex");
+      });
+    }
+
+    element.focus();
+  }
+};
--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -3,11 +3,13 @@ import { Application } from "@hotwired/stimulus";
 
 import { ErrorSummaryController } from "../components/error-summary/index.js";
 import { PreviewController } from "../components/preview/index.js";
+import { NotificationController } from "../components/notification/index.js";
 import { TextareaController } from "../components/textarea/index.js";
 import { TokenInputController } from "../components/token-input/index.js";
 
 window.Stimulus = Application.start();
 Stimulus.register("error-summary", ErrorSummaryController);
+Stimulus.register("notification", NotificationController);
 Stimulus.register("preview", PreviewController);
 Stimulus.register("textarea", TextareaController);
 Stimulus.register("token-input", TokenInputController);
