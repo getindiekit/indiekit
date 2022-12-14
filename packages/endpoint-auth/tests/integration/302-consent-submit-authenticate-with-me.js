f9a5c421583f823e8bd7e7caf46c5ff20c6a9a86
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-14 22:46:46 +0000

    test(endpoint-auth): complete test coverage

---
+++ packages/endpoint-auth/tests/integration/302-consent-submit-authenticate-with-me.js
@@ -0,0 +1,32 @@
+import process from "node:process";
+import test from "ava";
+import supertest from "supertest";
+import { mockAgent } from "@indiekit-test/mock-agent";
+import { testServer } from "@indiekit-test/server";
+import { createPasswordHash } from "../../lib/password.js";
+
+await mockAgent("auth-endpoint");
+
+test("Returns 302 submitting authenticated user", async (t) => {
+  process.env.PASSWORD_SECRET = await createPasswordHash("foo");
+  const server = await testServer();
+  const request = supertest.agent(server);
+  const authResponse = await request
+    .get("/auth")
+    .query({ client_id: "https://auth-endpoint.example" })
+    .query({ me: "https://website.example" })
+    .query({ redirect_uri: "https://auth-endpoint.example/redirect" })
+    .query({ response_type: "code" })
+    .query({ state: "12345" });
+  const reference = authResponse.headers.location.slice(-16);
+  const result = await request
+    .post("/auth/consent")
+    .type("form")
+    .query({ request_uri: `urn:ietf:params:oauth:request_uri:${reference}` })
+    .send({ password: "foo" });
+
+  t.is(result.status, 302);
+  t.regex(result.headers.location, /code=(.*)&iss=(.*)&state=(.*)&me=(.*)/);
+
+  server.close(t);
+});
---
+++ packages/endpoint-auth/tests/integration/422-consent-password-invalid.js
@@ -0,0 +1,38 @@
+import test from "ava";
+import supertest from "supertest";
+import { JSDOM } from "jsdom";
+import { mockAgent } from "@indiekit-test/mock-agent";
+import { testServer } from "@indiekit-test/server";
+
+await mockAgent("auth-endpoint");
+
+test("Returns 422 error missing password", async (t) => {
+  const server = await testServer();
+  const request = supertest.agent(server);
+  const authResponse = await request
+    .get("/auth")
+    .query({ client_id: "https://auth-endpoint.example" })
+    .query({ redirect_uri: "https://auth-endpoint.example/redirect" })
+    .query({ response_type: "code" })
+    .query({ state: "12345" });
+  const reference = authResponse.headers.location.slice(-16);
+  const response = await request
+    .post("/auth/consent")
+    .type("form")
+    .query({ request_uri: `urn:ietf:params:oauth:request_uri:${reference}` })
+    .send({ password: "foo" });
+  const dom = new JSDOM(response.text);
+  const result = dom.window.document;
+
+  t.is(response.status, 422);
+  t.is(
+    result.querySelector("title").textContent,
+    "Error: Sign in - Test configuration"
+  );
+  t.is(
+    result.querySelector("#password-error .error-message__text").textContent,
+    "Enter a valid password"
+  );
+
+  server.close(t);
+});
