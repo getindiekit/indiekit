daa8914ec367747f9851d827eedd429178e03dea
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-20 01:27:36 +0000

    feat(frontend): geo input

--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -2,6 +2,7 @@
 import { Application } from "@hotwired/stimulus";
 
 import { ErrorSummaryController } from "../components/error-summary/index.js";
+import { GeoInputController } from "../components/geo-input/index.js";
 import { PreviewController } from "../components/preview/index.js";
 import { NotificationController } from "../components/notification/index.js";
 import { TextareaController } from "../components/textarea/index.js";
@@ -9,6 +10,7 @@ import { TokenInputController } from "../components/token-input/index.js";
 
 window.Stimulus = Application.start();
 Stimulus.register("error-summary", ErrorSummaryController);
+Stimulus.register("geo-input", GeoInputController);
 Stimulus.register("notification", NotificationController);
 Stimulus.register("preview", PreviewController);
 Stimulus.register("textarea", TextareaController);
