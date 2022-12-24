9bb518f419719777ec1584008d0f6d28481b3e26
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2022-12-24 20:50:43 +0000

    test(frontend): improve summary row test

--- packages/frontend/tests/unit/globals/summary-row.js
+++ packages/frontend/tests/unit/globals/summary-row.js
@@ -3,11 +3,17 @@ import { summaryRows } from "../../../lib/globals/index.js";
 
 test("Transforms object into array for consumption by summary component", (t) => {
   const result = summaryRows({
-    me: "https://website.example",
-    categories: ["foo", "bar"],
+    array: ["foo", "bar"],
+    boolean: [true],
+    object: {
+      foo: "bar",
+    },
+    string: "string",
   });
 
-  t.is(result[0].key.text, "me");
-  t.regex(result[0].value.text, /^<code class="token string">/);
-  t.regex(result[1].value.text, /^<pre class="language-js">/);
+  t.is(result[0].key.text, "array");
+  t.regex(result[0].value.text, /^<pre class="language-js">/);
+  t.regex(result[1].value.text, /^<code class="token boolean">/);
+  t.regex(result[2].value.text, /^<pre class="language-js">/);
+  t.regex(result[3].value.text, /^<code class="token string">/);
 });
