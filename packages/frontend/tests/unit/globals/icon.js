bda5ff19982682a1c5da5ea6b9b53abea7196af5
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-05-18 23:34:41 +0100

    feat(frontend): return undefined for unknown icon name

--- packages/frontend/tests/unit/globals/icon.js
+++ packages/frontend/tests/unit/globals/icon.js
@@ -6,3 +6,7 @@ test("Renders SVG icon", (t) => {
 
   t.regex(result, /^<svg class="icon" xmlns="http:\/\/www.w3.org\/2000\/svg"/);
 });
+
+test("Returns undefined if unknown SVG icon name", (t) => {
+  t.is(icon("foo"), undefined);
+});
