edbfc15528ecfae6af9e489f21c31c96ea968340
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-05-19 00:27:12 +0100

    fix(endpoint-media): don’t require database to query config. fixes #600

--- packages/endpoint-media/lib/controllers/query.js
+++ packages/endpoint-media/lib/controllers/query.js
@@ -11,29 +11,10 @@ export const queryController = async (request, response, next) => {
   const { application, publication } = request.app.locals;
 
   try {
-    if (!application.hasDatabase) {
-      throw IndiekitError.notImplemented(
-        response.__("NotImplementedError.database")
-      );
-    }
-
     let { page, limit, offset } = request.query;
     page = Number.parseInt(page, 10) || 1;
     limit = Number.parseInt(limit, 10) || 18;
     offset = Number.parseInt(offset, 10) || (page - 1) * limit;
-
-    const files = await publication.media
-      .find()
-      .project({
-        "properties.content-type": 1,
-        "properties.post-type": 1,
-        "properties.published": 1,
-        "properties.url": 1,
-      })
-      .sort({ "properties.published": -1 })
-      .skip(offset)
-      .limit(limit)
-      .toArray();
 
     const { q, url } = request.query;
     if (!q) {
@@ -42,35 +23,51 @@ export const queryController = async (request, response, next) => {
       );
     }
 
-    let item;
-    if (url) {
-      item = await publication.media.findOne(
-        { "properties.url": url },
-        {
-          projection: {
-            "properties.content-type": 1,
-            "properties.post-type": 1,
-            "properties.published": 1,
-            "properties.url": 1,
-          },
-        }
-      );
-
-      if (!item) {
-        throw IndiekitError.notFound(
-          response.__("NotFoundError.resource", "file")
-        );
-      }
-    }
-
     switch (q) {
       case "source": {
+        if (!application.hasDatabase) {
+          throw IndiekitError.notImplemented(
+            response.__("NotImplementedError.database")
+          );
+        }
+
         // Return properties for a given source URL
         if (url) {
+          const item = await publication.media.findOne(
+            { "properties.url": url },
+            {
+              projection: {
+                "properties.content-type": 1,
+                "properties.post-type": 1,
+                "properties.published": 1,
+                "properties.url": 1,
+              },
+            }
+          );
+
+          if (!item) {
+            throw IndiekitError.notFound(
+              response.__("NotFoundError.resource", "file")
+            );
+          }
+
           return response.json(item.properties);
         }
 
         // Return properties for previously uploaded files
+        const files = await publication.media
+          .find()
+          .project({
+            "properties.content-type": 1,
+            "properties.post-type": 1,
+            "properties.published": 1,
+            "properties.url": 1,
+          })
+          .sort({ "properties.published": -1 })
+          .skip(offset)
+          .limit(limit)
+          .toArray();
+
         return response.json({
           _count: await publication.media.countDocuments(),
           items: files.map((media) => media.properties),
