a31fe106ae98800092099811f09c7dbf1a4cc0ae
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-05-23 00:50:48 +0100

    refactor(indiekit): improve request logging

--- packages/indiekit/lib/middleware/logging.js
+++ packages/indiekit/lib/middleware/logging.js
@@ -6,7 +6,7 @@ export const logging = (request, response, next) => {
   // Send debug logging output to console.info
   debug.log = console.info.bind(console);
 
-  debug("url", request.originalUrl);
+  debug("url", request.method, request.originalUrl);
   debug("headers", request.headers);
   debug("body", request.body);
 
