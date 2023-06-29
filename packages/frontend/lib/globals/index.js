2f3f645a6eca9ce7b4b8789042baaf5ee15364e8
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-06-29 23:21:39 +0100

    feat(frontend): remove pages global

--- packages/frontend/lib/globals/index.js
+++ packages/frontend/lib/globals/index.js
@@ -3,6 +3,5 @@ export { classes } from "./classes.js";
 export { errorList } from "./error-list.js";
 export { icon } from "./icon.js";
 export { itemId } from "./item-id.js";
-export { pages } from "./pages.js";
 export { summaryRows } from "./summary-rows.js";
 export { themeCustomProperties } from "./theme.js";
