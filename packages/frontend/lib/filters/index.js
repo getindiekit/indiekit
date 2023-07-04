04f0cea13d9c14856d51d45888e330c3f6176d5d
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-04 23:59:34 +0100

    refactor(frontend): use slugify from util package

--- packages/frontend/lib/filters/index.js
+++ packages/frontend/lib/filters/index.js
@@ -1,5 +1,4 @@
-export { getCanonicalUrl as url } from "@indiekit/util";
-export { default as slugify } from "@sindresorhus/slugify";
+export { slugify, getCanonicalUrl as url } from "@indiekit/util";
 export { default as widont } from "widont";
 export { date, languageName, languageNativeName } from "./locale.js";
 export { excerpt, includes, linkTo, markdown } from "./string.js";
