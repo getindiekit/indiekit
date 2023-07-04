33d8fcf2e1186f278fe4ce8f0b0fc9427192a481
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-04 19:36:01 +0100

    refactor(endpoint-auth): use isUrl from util package

--- packages/endpoint-auth/lib/utils.js
+++ packages/endpoint-auth/lib/utils.js
@@ -28,21 +28,3 @@ export const getRequestParameters = (request) => {
 
   return request.query;
 };
-
-/**
- * Check if given string is a valid URL
- * @param {object} string - URL
- * @returns {boolean} String is a URL
- */
-export const isUrl = (string) => {
-  if (typeof string !== "string") {
-    throw new TypeError("Expected a string");
-  }
-
-  try {
-    new URL(decodeURIComponent(string)); // eslint-disable-line no-new
-    return true;
-  } catch {
-    return false;
-  }
-};
