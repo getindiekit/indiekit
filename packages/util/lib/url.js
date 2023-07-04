bd14d3b04d14e1e6862f7bbb983cfc969cba7483
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-04 20:12:19 +0100

    feat(util): isSameOrigin

--- packages/util/lib/url.js
+++ packages/util/lib/url.js
@@ -1,4 +1,17 @@
 /**
+ * Check if parsed URL string has given origin
+ * @param {string} string - URL, i.e. https://website.example:80/path
+ * @param {string} origin - Origin, i.e. https://website.example:80
+ * @returns {boolean} String is a URL with same origin
+ * @see {@link https://developer.mozilla.org/en-US/docs/Web/Security/Same-origin_policy}
+ */
+export const isSameOrigin = (string, origin) => {
+  const url = new URL(string);
+  const originUrl = new URL(origin);
+  return url.origin === originUrl.origin;
+};
+
+/**
  * Check if given string is a valid URL
  * @param {object} string - URL
  * @returns {boolean} String is a URL
--- packages/util/tests/unit/url.js
+++ packages/util/tests/unit/url.js
@@ -1,5 +1,15 @@
 import test from "ava";
-import { isUrl } from "../../lib/url.js";
+import { isSameOrigin, isUrl } from "../../lib/url.js";
+
+test("Checks if parsed URL string has given origin", (t) => {
+  t.true(
+    isSameOrigin(
+      "https://mastodon.example/@username/1234567890987654321",
+      "https://mastodon.example"
+    )
+  );
+  t.false(isSameOrigin("https://getindiekit.com", "https://mastodon.example"));
+});
 
 test("Checks if given string is a valid URL", (t) => {
   t.true(isUrl("https%3A%2F%2Ffoo.bar"));
