52501dc5cbddcc63095ffedbb81eba2def11ff78
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-04 21:42:59 +0100

    feat(util): getCanonicalUrl

--- packages/util/tests/unit/url.js
+++ packages/util/tests/unit/url.js
@@ -1,5 +1,21 @@
 import test from "ava";
-import { isSameOrigin, isUrl } from "../../lib/url.js";
+import { getCanonicalUrl, isSameOrigin, isUrl } from "../../lib/url.js";
+
+test("Gets canonical URL", (t) => {
+  t.is(getCanonicalUrl("https://website.example"), "https://website.example/");
+  t.is(
+    getCanonicalUrl("path1", "https://website.example"),
+    "https://website.example/path1"
+  );
+  t.is(
+    getCanonicalUrl("/path2", "https://website.example"),
+    "https://website.example/path2"
+  );
+  t.is(
+    getCanonicalUrl("https://website.example/path3", "https://website.example"),
+    "https://website.example/path3"
+  );
+});
 
 test("Checks if parsed URL string has given origin", (t) => {
   t.true(
