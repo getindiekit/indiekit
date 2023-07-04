1b2f833efc5d431442ce7b399b846ae852591868
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-04 22:21:58 +0100

    refactor(frontend): use getCanonicalUrl from util package

--- packages/frontend/lib/filters/index.js
+++ packages/frontend/lib/filters/index.js
@@ -1,5 +1,6 @@
+export { getCanonicalUrl as url } from "@indiekit/util";
 export { default as slugify } from "@sindresorhus/slugify";
 export { default as widont } from "widont";
 export { date, languageName, languageNativeName } from "./locale.js";
 export { excerpt, includes, linkTo, markdown } from "./string.js";
-export { absoluteUrl, friendlyUrl, imageUrl } from "./url.js";
+export { friendlyUrl, imageUrl } from "./url.js";
