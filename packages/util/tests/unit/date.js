febf338d01a1e3c400c32712da1ba6f257a5ae39
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-05 13:11:49 +0100

    feat(util): getServerTimeZone

--- packages/util/tests/unit/date.js
+++ packages/util/tests/unit/date.js
@@ -1,5 +1,6 @@
+import process from "node:process";
 import test from "ava";
-import { formatDate } from "../../lib/date.js";
+import { formatDate, getServerTimeZone } from "../../lib/date.js";
 
 test("Formats a date", (t) => {
   t.is(formatDate("2019-11-30", "dd MMMM yyyy"), "30 November 2019");
@@ -13,4 +14,18 @@ test("Formats the date right now", (t) => {
   const result = Math.round(formatDate("now", "t") / 1000);
 
   t.is(result, now);
+});
+
+test("Gets server timezone offset", (t) => {
+  process.env.TZ = "Asia/Taipei"; // Does not observe DST
+  const ahead = getServerTimeZone();
+  t.is(ahead, "+08:00");
+
+  process.env.TZ = "America/Panama"; // Does not observe DS
+  const behind = getServerTimeZone();
+  t.is(behind, "-05:00");
+
+  process.env.TZ = "UTC";
+  const utc = getServerTimeZone();
+  t.is(utc, "Z");
 });
