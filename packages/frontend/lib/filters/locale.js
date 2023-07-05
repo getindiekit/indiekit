ace7d93d82fb3c7b4664d7d94c95bcb1a4cb0ad5
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-05 12:10:36 +0100

    refactor(frontend): use formatDate from util package

--- packages/frontend/lib/filters/locale.js
+++ packages/frontend/lib/filters/locale.js
@@ -1,22 +1,4 @@
-import dateFns from "date-fns";
-import locales from "date-fns/locale/index.js";
 import languages from "iso-639-1";
-
-const { format, parseISO } = dateFns;
-
-/**
- * Format a date
- * @param {string} string - ISO 8601 date
- * @param {string} tokens - Tokenised date format
- * @param {string} locale - ISO 639-1 (plus optional country code)
- * @returns {string} Formatted date
- */
-export const date = (string, tokens, locale = "en") => {
-  locale = locales[locale.replace("-", "")];
-  const date = string === "now" ? new Date() : parseISO(string);
-  const dateTime = format(date, tokens, { locale });
-  return dateTime;
-};
 
 /**
  * Get a language name
