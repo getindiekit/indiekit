1feaa887db076228df9641e1f32572348a559193
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-10 22:40:40 +0100

    refactor(helpers): test token options optional

--- helpers/access-token/index.js
+++ helpers/access-token/index.js
@@ -3,13 +3,13 @@ import { signToken } from "@indiekit/endpoint-auth/lib/token.js";
 
 /**
  * Generate access token for testing
- * @param {object} options - Options
+ * @param {object} [options] - Options
  * @param {boolean} [options.invalid] - Invalidate token
  * @param {string} [options.me] - Publication URL
  * @param {string} [options.scope] - Permissions scope
  * @returns {object} Access token
  */
-export const testToken = (options) => {
+export const testToken = (options = {}) => {
   const expiresIn = options?.invalid ? "0s" : "10m";
   const me = options?.me || "https://website.example";
   const scope = options?.scope || "create update delete media";
