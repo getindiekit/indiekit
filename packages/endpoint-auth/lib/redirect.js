3a1aa65e1bea539aef056255a48e610c18e1178c
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-14 20:11:26 +0100

    style: lint js doc

--- packages/endpoint-auth/lib/redirect.js
+++ packages/endpoint-auth/lib/redirect.js
@@ -1,5 +1,8 @@
 /**
  * Validate `redirect_uri`
+ * @param {string} redirectUri - Redirect URL
+ * @param {string} clientId - URL of client
+ * @returns {boolean} Valid redirect
  * @see {@link https://indieauth.spec.indieweb.org/#redirect-url}
  * @todo If redirect URIs doesn’t share same host as `client_id`, validate
  * against list of redirect URIs fetched from `<link>` tags or `Link` HTTP
