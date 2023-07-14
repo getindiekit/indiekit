2c96a0395a7bfb9962312f6d53a4cc420e65a672
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-14 20:19:52 +0100

    test(endpoint-posts): update test with no database

---
+++ packages/endpoint-posts/tests/integration/200-posts-no-database.js
@@ -0,0 +1,21 @@
+import test from "ava";
+import supertest from "supertest";
+import { JSDOM } from "jsdom";
+import { testServer } from "@indiekit-test/server";
+import { cookie } from "@indiekit-test/session";
+
+test("Returns no published posts (no database)", async (t) => {
+  const server = await testServer({ useDatabase: false });
+  const request = supertest.agent(server);
+  const response = await request.get("/posts").set("cookie", [cookie()]);
+  const dom = new JSDOM(response.text);
+  const result = dom.window.document;
+
+  t.is(
+    result.querySelector("title").textContent,
+    "Published posts - Test configuration"
+  );
+  t.regex(result.querySelector(".main__container p").textContent, /No posts/);
+
+  server.close(t);
+});
