5ee9c1a3265b80d095b6d345d5647644b221bd23
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-15 19:14:58 +0100

    fix(endpoint-auth): improve client information discovery. fixes #626

--- packages/endpoint-auth/tests/unit/client.js
+++ packages/endpoint-auth/tests/unit/client.js
@@ -4,26 +4,39 @@ import { getClientInformation } from "../../lib/client.js";
 
 await mockAgent("endpoint-auth");
 
-test("Gets client information (h-app)", async (t) => {
-  const result = await getClientInformation("https://auth-endpoint.example");
-
-  t.deepEqual(result, {
-    name: "Example website",
-    url: "https://auth-endpoint.example",
-  });
-});
-
-test("Gets client information (h-x-app with logo)", async (t) => {
-  const result = await getClientInformation("https://auth-endpoint.example");
+test("Gets client information (with h-x-app microformat)", async (t) => {
+  const result = await getClientInformation("https://auth-endpoint.example/");
 
   t.deepEqual(result, {
     logo: "https://auth-endpoint.example/assets/icon.svg",
-    name: "Example website",
-    url: "https://auth-endpoint.example",
+    name: "Example client",
+    url: "https://auth-endpoint.example/",
   });
 });
 
-test("Returns default client information if no response", async (t) => {
+test("Gets client information (without h-x-app microformat)", async (t) => {
+  const result = await getClientInformation(
+    "https://auth-endpoint.example/mf2"
+  );
+
+  t.deepEqual(result, {
+    name: "auth-endpoint.example",
+    url: "https://auth-endpoint.example/mf2",
+  });
+});
+
+test("Gets client information (without microformats)", async (t) => {
+  const result = await getClientInformation(
+    "https://auth-endpoint.example/no-mf2"
+  );
+
+  t.deepEqual(result, {
+    name: "auth-endpoint.example",
+    url: "https://auth-endpoint.example/no-mf2",
+  });
+});
+
+test("Returns client information (no response)", async (t) => {
   const result = await getClientInformation(
     "https://auth-endpoint.example/404"
   );
