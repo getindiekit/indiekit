a8b5a8b7f8b1f29fe5e733864618e674b769ae2c
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-26 23:03:01 +0100

    refactor(endpoint-files): remove unnecessary try/catch

--- packages/endpoint-files/lib/controllers/file.js
+++ packages/endpoint-files/lib/controllers/file.js
@@ -4,29 +4,25 @@ import path from "node:path";
  * View uploaded file
  * @type {import("express").RequestHandler}
  */
-export const fileController = async (request, response, next) => {
-  try {
-    const { file, fileName, filesPath, scope } = response.locals;
+export const fileController = async (request, response) => {
+  const { file, fileName, filesPath, scope } = response.locals;
 
-    response.render("file", {
-      title: fileName,
-      file,
-      parent: {
-        href: filesPath,
-        text: response.locals.__("files.files.title"),
-      },
-      actions: [
-        scope.includes("delete")
-          ? {
-              classes: "actions__link--warning",
-              href: path.join(request.baseUrl + request.path, "/delete"),
-              icon: "delete",
-              text: response.locals.__("files.delete.action"),
-            }
-          : {},
-      ],
-    });
-  } catch (error) {
-    next(error);
-  }
+  response.render("file", {
+    title: fileName,
+    file,
+    parent: {
+      href: filesPath,
+      text: response.locals.__("files.files.title"),
+    },
+    actions: [
+      scope.includes("delete")
+        ? {
+            classes: "actions__link--warning",
+            href: path.join(request.baseUrl + request.path, "/delete"),
+            icon: "delete",
+            text: response.locals.__("files.delete.action"),
+          }
+        : {},
+    ],
+  });
 };
