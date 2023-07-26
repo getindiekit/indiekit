6c34e9c14e9a0c32125fd73e42c34825c5b0c840
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-07-26 22:44:43 +0100

    test(endpoint-media): source query items

--- packages/endpoint-media/tests/integration/200-query-source.js
+++ packages/endpoint-media/tests/integration/200-query-source.js
@@ -1,18 +1,30 @@
 import test from "ava";
 import supertest from "supertest";
+import { getFixture } from "@indiekit-test/fixtures";
 import { testServer } from "@indiekit-test/server";
 import { testToken } from "@indiekit-test/token";
 
 test("Returns list of uploaded files", async (t) => {
   const server = await testServer();
   const request = supertest.agent(server);
+
+  // Upload file
+  await request
+    .post("/media")
+    .auth(testToken(), { type: "bearer" })
+    .set("accept", "application/json")
+    .attach("file", getFixture("file-types/photo.jpg", false), "photo.jpg");
+
   const result = await request
     .get("/media")
     .auth(testToken(), { type: "bearer" })
     .set("accept", "application/json")
     .query({ q: "source" });
 
-  t.truthy(result.body.items);
+  t.is(result.body.items[0]["content-type"], "image/jpeg");
+  t.is(result.body.items[0]["media-type"], "photo");
+  t.truthy(result.body.items[0].published);
+  t.true(result.body.items[0].url.startsWith("https://website.example"));
 
   server.close(t);
 });
