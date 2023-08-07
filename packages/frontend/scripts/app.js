e0937b6c02f2b80965c8ed3800df30486169a618
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-07 21:47:10 +0100

    feat(frontend): add another component

--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -1,5 +1,6 @@
 /* global Stimulus */
 import { Application } from "@hotwired/stimulus";
+import { AddAnotherController } from "../components/add-another/index.js";
 import { ErrorSummaryController } from "../components/error-summary/index.js";
 import { GeoInputController } from "../components/geo-input/index.js";
 import { PreviewController } from "../components/preview/index.js";
@@ -8,6 +9,7 @@ import { TagInputController } from "../components/tag-input/index.js";
 import { TextareaController } from "../components/textarea/index.js";
 
 window.Stimulus = Application.start();
+Stimulus.register("add-another", AddAnotherController);
 Stimulus.register("error-summary", ErrorSummaryController);
 Stimulus.register("geo-input", GeoInputController);
 Stimulus.register("notification", NotificationController);
