9c8fe7a1841f6b5931185b89328d2b87eeb600f8
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-25 22:22:59 +0100

    style(frontend): syntax for geo input component

--- packages/frontend/components/geo-input/index.js
+++ packages/frontend/components/geo-input/index.js
@@ -1,8 +1,9 @@
+/* eslint-disable jsdoc/no-undefined-types */
 import { Controller } from "@hotwired/stimulus";
 import { wrapElement } from "../../lib/utils/wrap-element.js";
 
 export const GeoInputController = class extends Controller {
-  static targets = ["errorMessage", "geo"];
+  static targets = ["errorMessage", "findButton", "geo"];
 
   static values = {
     denied: String,
@@ -15,85 +16,82 @@ export const GeoInputController = class extends Controller {
     }
 
     // Create group to hold input and button
-    const inputButtonGroup = document.createElement("div");
-    inputButtonGroup.classList.add("input-button-group");
+    const $inputButtonGroup = document.createElement("div");
+    $inputButtonGroup.classList.add("input-button-group");
 
     // Create and display find location button
-    const buttonTemplate = document.querySelector("#geo-input-button");
-    const button = buttonTemplate.content.cloneNode(true);
-    const input = this.element.querySelector(".input");
+    const $button = this.findButtonTarget.content.cloneNode(true);
+    const $input = this.element.querySelector(".input");
 
     // Wrap input within `input-button-group` container
-    wrapElement(input, inputButtonGroup);
+    wrapElement($input, $inputButtonGroup);
 
     // Add button to `input-button-group` container
-    inputButtonGroup.append(button);
+    $inputButtonGroup.append($button);
   }
 
-  showError(element, message) {
-    const input = element.querySelector(".input");
-    const inputButtonGroup = element.querySelector(".input-button-group");
-    const inputField = element.querySelector(".field");
-
-    // Create error message
-    const errorMessageTemplate = document.querySelector(
-      "#geo-input-error-message",
-    );
-    const errorMessageElement = errorMessageTemplate.content.cloneNode(true);
-    inputButtonGroup.before(errorMessageElement);
-    const errorMessage = element.querySelector(".error-message");
-    const errorMessageText = element.querySelector(".error-message__text");
-
-    // Add field error class
-    inputField.classList.add("field--error");
-
-    // Add error message text
-    errorMessageText.textContent = message;
-
-    // Update `aria-describedby` on input element to reference error message
-    const inputAttributes = input.getAttribute("aria-describedby") || "";
-    input.setAttribute(
-      "aria-describedby",
-      [inputAttributes, errorMessage.id].join(" "),
+  /**
+   * Get current geo location position
+   */
+  find() {
+    navigator.geolocation.getCurrentPosition(
+      this.positionSuccess.bind(this),
+      this.positionError.bind(this),
+      {
+        enableHighAccuracy: true,
+        timeout: 5000,
+      },
     );
   }
 
   /**
-   * @typedef GeolocationPosition
-   * @property {object} position - Position
-   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPosition}
-   * @typedef GeolocationPositionError
-   * @property {object} error - Error
-   * @see {@link https://developer.mozilla.org/en-US/docs/Web/API/GeolocationPositionError}
+   * Populate input with geo location position
+   * @param {GeolocationPosition} position - Position
    */
-  find() {
-    const { element, geoTarget, deniedValue, failedValue, showError } = this;
+  positionSuccess(position) {
+    const latitude = position.coords.latitude.toFixed(5);
+    const longitude = position.coords.longitude.toFixed(5);
+    this.geoTarget.value = [latitude, longitude].join(",");
+  }
 
-    /**
-     * @param {GeolocationPosition} position - Position
-     */
-    function success(position) {
-      const latitude = position.coords.latitude.toFixed(5);
-      const longitude = position.coords.longitude.toFixed(5);
-      geoTarget.value = [latitude, longitude].join(",");
+  /**
+   * Display geo location position error message
+   * @param {GeolocationPositionError} error - Position error
+   */
+  positionError(error) {
+    /** @satisfies {HTMLButtonElement} */
+    const $button = this.element.querySelector(".button");
+
+    $button.disabled = true;
+
+    if (error.PERMISSION_DENIED) {
+      this.showErrorMessage(this.element, this.deniedValue);
+    } else if (error.POSITION_UNAVAILABLE) {
+      this.showErrorMessage(this.element, this.failedValue);
     }
+  }
 
-    /**
-     * @param {GeolocationPositionError} error - Position error
-     */
-    function error(error) {
-      element.querySelector(".button").disabled = true;
+  showErrorMessage($field, message) {
+    const $input = $field.querySelector(".input");
+    const $inputButtonGroup = $field.querySelector(".input-button-group");
 
-      if (error.PERMISSION_DENIED) {
-        showError(element, deniedValue);
-      } else if (error.POSITION_UNAVAILABLE) {
-        showError(element, failedValue);
-      }
-    }
+    // Create error message
+    let $errorMessage = this.errorMessageTarget.content.cloneNode(true);
+    $inputButtonGroup.before($errorMessage);
+    $errorMessage = $field.querySelector(".error-message");
+    const $errorMessageText = $field.querySelector(".error-message__text");
 
-    navigator.geolocation.getCurrentPosition(success, error, {
-      enableHighAccuracy: true,
-      timeout: 5000,
-    });
+    // Add field error class
+    $field.classList.add("field--error");
+
+    // Add error message text
+    $errorMessageText.textContent = message;
+
+    // Update `aria-describedby` on input element to reference error message
+    const inputAttributes = $input.getAttribute("aria-describedby") || "";
+    $input.setAttribute(
+      "aria-describedby",
+      [inputAttributes, $errorMessage.id].join(" "),
+    );
   }
 };
