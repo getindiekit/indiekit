5c29ef0ce66d2b1ba94ddb7584247b4e71c8edba
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-25 22:32:58 +0100

    style(frontend): syntax for wrap element utility

--- packages/frontend/lib/utils/wrap-element.js
+++ packages/frontend/lib/utils/wrap-element.js
@@ -1,11 +1,11 @@
 /**
  * Wrap element with another
- * @param {HTMLElement} element - Element to wrap
- * @param {HTMLElement} wrapper - Element to wrap with
+ * @param {HTMLElement} $element - Element to wrap
+ * @param {HTMLElement} $wrapper - Element to wrap with
  */
-export const wrapElement = (element, wrapper) => {
-  if (element?.parentNode) {
-    element.parentNode.insertBefore(wrapper, element);
-    wrapper.append(element);
+export const wrapElement = ($element, $wrapper) => {
+  if ($element?.parentNode) {
+    $element.parentNode.insertBefore($wrapper, $element);
+    $wrapper.append($element);
   }
 };
