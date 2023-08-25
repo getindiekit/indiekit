cbd57bd221133573b293d4fadd4a6f69056c2aff
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-25 23:13:25 +0100

    feat(frontend): conditional checkboxes

--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -1,6 +1,7 @@
 /* global Stimulus */
 import { Application } from "@hotwired/stimulus";
 import { AddAnotherController } from "../components/add-another/index.js";
+import { CheckboxesController } from "../components/checkboxes/index.js";
 import { ErrorSummaryController } from "../components/error-summary/index.js";
 import { GeoInputController } from "../components/geo-input/index.js";
 import { PreviewController } from "../components/preview/index.js";
@@ -11,6 +12,7 @@ import { TextareaController } from "../components/textarea/index.js";
 
 window.Stimulus = Application.start();
 Stimulus.register("add-another", AddAnotherController);
+Stimulus.register("checkboxes", CheckboxesController);
 Stimulus.register("error-summary", ErrorSummaryController);
 Stimulus.register("geo-input", GeoInputController);
 Stimulus.register("notification", NotificationController);
