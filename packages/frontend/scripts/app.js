be4c0550bacb02c536cc36c7f24139d7d2f1c037
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-08-25 16:35:36 +0100

    feat(frontend): conditional radios

--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -5,6 +5,7 @@ import { ErrorSummaryController } from "../components/error-summary/index.js";
 import { GeoInputController } from "../components/geo-input/index.js";
 import { PreviewController } from "../components/preview/index.js";
 import { NotificationController } from "../components/notification/index.js";
+import { RadiosController } from "../components/radios/index.js";
 import { TagInputController } from "../components/tag-input/index.js";
 import { TextareaController } from "../components/textarea/index.js";
 
@@ -14,5 +15,6 @@ Stimulus.register("error-summary", ErrorSummaryController);
 Stimulus.register("geo-input", GeoInputController);
 Stimulus.register("notification", NotificationController);
 Stimulus.register("preview", PreviewController);
+Stimulus.register("radios", RadiosController);
 Stimulus.register("tag-input", TagInputController);
 Stimulus.register("textarea", TextareaController);
