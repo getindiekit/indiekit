ada82e6f80dae176c633e2b73d06f41c728cbf00
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-10-06 23:43:10 +0100

    chore: update dependencies

--- packages/frontend/lib/rollup.js
+++ packages/frontend/lib/rollup.js
@@ -2,16 +2,11 @@ import { fileURLToPath } from "node:url";
 import { rollup } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
-import nodePolyfills from "rollup-plugin-polyfill-node";
 
 export const scripts = async () => {
   const bundle = await rollup({
     input: fileURLToPath(new URL("../scripts/app.js", import.meta.url)),
-    plugins: [
-      commonjs(),
-      nodeResolve({ preferBuiltins: true }),
-      nodePolyfills(),
-    ],
+    plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
   });
 
   const { output } = await bundle.generate({ format: "iife", name: "app" });
