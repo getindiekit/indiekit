580dd5abb11f2bba0b73bff0f2e10f4fbe4aec89
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-10-21 16:00:18 +0100

    refactor(endpoint-micropub): use destructuring assignment for post content values

--- packages/endpoint-micropub/lib/post-content.js
+++ packages/endpoint-micropub/lib/post-content.js
@@ -7,23 +7,24 @@ export const postContent = {
    */
   async create(publication, postData) {
     const { postTemplate, store, storeMessageTemplate } = publication;
+    const { path, properties } = postData;
     const metaData = {
       action: "create",
       result: "created",
       fileType: "post",
-      postType: postData.properties["post-type"],
+      postType: properties["post-type"],
     };
-    const content = await postTemplate(postData.properties);
+    const content = await postTemplate(properties);
     const message = storeMessageTemplate(metaData);
 
-    await store.createFile(postData.path, content, { message });
+    await store.createFile(path, content, { message });
 
     return {
-      location: postData.properties.url,
+      location: properties.url,
       status: 202,
       json: {
         success: "create_pending",
-        success_description: `Post will be created at ${postData.properties.url}`,
+        success_description: `Post will be created at ${properties.url}`,
       },
     };
   },
@@ -37,16 +38,16 @@ export const postContent = {
    */
   async update(publication, postData, url) {
     const { postTemplate, store, storeMessageTemplate } = publication;
+    const { _originalPath, path, properties } = postData;
     const metaData = {
       action: "update",
       result: "updated",
       fileType: "post",
-      postType: postData.properties["post-type"],
+      postType: properties["post-type"],
     };
-    const content = await postTemplate(postData.properties);
+    const content = await postTemplate(properties);
     const message = storeMessageTemplate(metaData);
-    const hasUpdatedUrl = url !== postData.properties.url;
-    const { _originalPath, path, properties } = postData;
+    const hasUpdatedUrl = url !== properties.url;
 
     _originalPath === path
       ? await store.updateFile(path, content, { message })
@@ -77,21 +78,22 @@ export const postContent = {
    */
   async delete(publication, postData) {
     const { store, storeMessageTemplate } = publication;
+    const { path, properties } = postData;
     const metaData = {
       action: "delete",
       result: "deleted",
       fileType: "post",
-      postType: postData.properties["post-type"],
+      postType: properties["post-type"],
     };
     const message = storeMessageTemplate(metaData);
 
-    await store.deleteFile(postData.path, { message });
+    await store.deleteFile(path, { message });
 
     return {
       status: 200,
       json: {
         success: "delete",
-        success_description: `Post deleted from ${postData.properties.url}`,
+        success_description: `Post deleted from ${properties.url}`,
       },
     };
   },
@@ -104,24 +106,24 @@ export const postContent = {
    */
   async undelete(publication, postData) {
     const { postTemplate, store, storeMessageTemplate } = publication;
-
+    const { path, properties } = postData;
     const metaData = {
       action: "undelete",
       result: "undeleted",
       fileType: "post",
-      postType: postData.properties["post-type"],
+      postType: properties["post-type"],
     };
-    const content = await postTemplate(postData.properties);
+    const content = await postTemplate(properties);
     const message = storeMessageTemplate(metaData);
 
-    await store.updateFile(postData.path, content, { message });
+    await store.updateFile(path, content, { message });
 
     return {
-      location: postData.properties.url,
+      location: properties.url,
       status: 200,
       json: {
         success: "delete_undelete",
-        success_description: `Post restored to ${postData.properties.url}`,
+        success_description: `Post restored to ${properties.url}`,
       },
     };
   },
