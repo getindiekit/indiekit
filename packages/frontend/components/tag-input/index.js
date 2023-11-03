beb08bd4eec1bacce178e1641fc6a77ab80f23b5
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-03 22:41:50 +0000

    refactor(frontend): use custom element for tag-input-field component

--- packages/frontend/components/tag-input/index.js
+++ packages/frontend/components/tag-input/index.js
@@ -1,62 +1,36 @@
-import { Controller } from "@hotwired/stimulus";
 import TagInput from "@accessible-components/tag-input";
 
-export const TagInputController = class extends Controller {
-  static values = {
-    tag: String,
-    placeholder: String,
-    edit: String,
-    delete: String,
-    added: String,
-    updated: String,
-    deleted: String,
-    selected: String,
-    noneSelected: String,
-    instruction: String,
-  };
-
-  initialize() {
-    const {
-      element,
-      addedValue,
-      deleteValue,
-      deletedValue,
-      editValue,
-      instructionValue,
-      noneSelectedValue,
-      placeholderValue,
-      selectedValue,
-      tagValue,
-      updatedValue,
-    } = this;
-
-    const $replacedField = element.querySelector(".field");
-    const $replacedInput = element.querySelector(".input");
-    const $replacedLabel = element.querySelector(".label");
+export const TagInputFieldComponent = class extends HTMLElement {
+  connectedCallback() {
+    const $hint = this.querySelector(".hint");
+    const $replacedInput = this.querySelector(".input");
+    const $replacedLabel = this.querySelector(".label");
 
     const value = $replacedInput.getAttribute("value");
     const tags = value ? $replacedInput.getAttribute("value").split(",") : [];
 
-    const tagInput = new TagInput(element, {
-      ariaTag: tagValue,
-      ariaEditTag: editValue,
-      ariaDeleteTag: deleteValue,
-      ariaTagAdded: addedValue,
-      ariaTagDeleted: deletedValue,
-      ariaTagUpdated: updatedValue,
-      ariaTagSelected: selectedValue,
-      ariaNoTagsSelected: noneSelectedValue,
-      ariaInputLabel: instructionValue,
+    const tagInput = new TagInput(this, {
+      ariaTag: this.getAttribute("tag"),
+      ariaEditTag: this.getAttribute("edit"),
+      ariaDeleteTag: this.getAttribute("delete"),
+      ariaTagAdded: this.getAttribute("added"),
+      ariaTagDeleted: this.getAttribute("deleted"),
+      ariaTagUpdated: this.getAttribute("updated"),
+      ariaTagSelected: this.getAttribute("selected"),
+      ariaNoTagsSelected: this.getAttribute("none-selected"),
+      ariaInputLabel: this.getAttribute("instruction"),
       disabled: $replacedInput.getAttribute("disabled"),
       label: $replacedLabel.innerHTML,
       name: $replacedInput.getAttribute("name"),
-      placeholder: placeholderValue,
+      placeholder: this.getAttribute("placeholder"),
       tags,
     });
 
-    element.querySelector(".tag-input-label").classList.add("label");
+    this.insertBefore($hint, this.querySelector(".tag-input"));
+    this.querySelector(".tag-input-label").classList.add("label");
 
-    $replacedField.remove();
+    $replacedLabel.remove();
+    $replacedInput.remove();
 
     return tagInput;
   }
