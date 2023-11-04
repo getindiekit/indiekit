8263e7282b877301e8b272057496cc2f23757626
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-04 21:51:20 +0000

    refactor(frontend): use custom element for textarea-field component

--- packages/frontend/components/textarea/index.js
+++ packages/frontend/components/textarea/index.js
@@ -1,29 +1,31 @@
-import { Controller } from "@hotwired/stimulus";
 import { debounce } from "../../lib/utils/debounce.js";
 
-export const TextareaController = class extends Controller {
-  initialize() {
+export const TextareaFieldComponent = class extends HTMLElement {
+  constructor() {
+    super();
+
     this.adjustHeight = this.adjustHeight.bind(this);
+    this.textarea = this.querySelector("textarea");
   }
 
-  connect() {
+  connectedCallback() {
     const delay = 100;
 
-    this.element.style.overflow = "hidden";
+    this.textarea.style.overflow = "hidden";
     this.onResize =
       delay > 0 ? debounce(this.adjustHeight, delay) : this.adjustHeight;
     this.adjustHeight();
 
-    this.element.addEventListener("input", this.adjustHeight);
+    this.textarea.addEventListener("input", this.adjustHeight);
     window.addEventListener("resize", this.onResize);
   }
 
-  disconnect() {
+  disconnectedCallback() {
     window.removeEventListener("resize", this.onResize);
   }
 
   adjustHeight() {
-    this.element.style.height = "auto";
-    this.element.style.height = `${this.element.scrollHeight + 4}px`;
+    this.textarea.style.height = "auto";
+    this.textarea.style.height = `${this.textarea.scrollHeight + 4}px`;
   }
 };
