29e82e2fb6336b39f8430435073d515c02d04d48
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-04 20:56:31 +0000

    refactor(frontend): use custom element for add-another component

--- packages/frontend/scripts/app.js
+++ packages/frontend/scripts/app.js
@@ -1,6 +1,6 @@
 /* global Stimulus */
 import { Application } from "@hotwired/stimulus";
-import { AddAnotherController } from "../components/add-another/index.js";
+import { AddAnotherComponent } from "../components/add-another/index.js";
 import { CheckboxesController } from "../components/checkboxes/index.js";
 import { ErrorSummaryController } from "../components/error-summary/index.js";
 import { GeoInputController } from "../components/geo-input/index.js";
@@ -11,7 +11,7 @@ import { TagInputController } from "../components/tag-input/index.js";
 import { TextareaController } from "../components/textarea/index.js";
 
 window.Stimulus = Application.start();
-Stimulus.register("add-another", AddAnotherController);
+customElements.define("add-another", AddAnotherComponent);
 Stimulus.register("checkboxes", CheckboxesController);
 Stimulus.register("error-summary", ErrorSummaryController);
 Stimulus.register("geo-input", GeoInputController);
