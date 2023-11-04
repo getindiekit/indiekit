1f8a62db6cc2cc5243f2c0e5930f1c565e3918d4
Paul Robert Lloyd <paulrobertlloyd@users.noreply.github.com>
2023-11-04 16:26:30 +0000

    refactor(frontend): use custom element for notification-banner component

--- packages/frontend/components/notification-banner/index.js
+++ packages/frontend/components/notification-banner/index.js
@@ -1,26 +1,24 @@
-import { Controller } from "@hotwired/stimulus";
-
-export const NotificationBannerController = class extends Controller {
-  initialize() {
-    if (this.element.dataset.disableAutoFocus === "true") {
+export const NotificationBannerComponent = class extends HTMLElement {
+  connectedCallback() {
+    if (this.getAttribute("disable-auto-focus") === "true") {
       return;
     }
 
-    if (this.element.getAttribute("role") !== "alert") {
+    if (this.getAttribute("role") !== "alert") {
       return;
     }
 
     // Set tabindex to -1 to make element focusable with JavaScript.
     // Remove tabindex on blur as component doesn’t need to be focusable
     // after page has loaded.
-    if (!this.element.getAttribute("tabindex")) {
-      this.element.setAttribute("tabindex", "-1");
+    if (!this.getAttribute("tabindex")) {
+      this.setAttribute("tabindex", "-1");
 
-      this.element.addEventListener("blur", function () {
-        this.element.removeAttribute("tabindex");
+      this.addEventListener("blur", function () {
+        this.removeAttribute("tabindex");
       });
     }
 
-    this.element.focus();
+    this.focus();
   }
 };
