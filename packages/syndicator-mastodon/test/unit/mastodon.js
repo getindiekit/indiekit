c2c0d4bfc09800ca916be4c7cc085ecce734f6ab
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2023-12-24 22:23:23 +0000

    test(syndicator-mastodon): use node test runner

---
+++ packages/syndicator-mastodon/test/unit/mastodon.js
@@ -0,0 +1,198 @@
+import { strict as assert } from "node:assert";
+import { beforeEach, describe, it } from "node:test";
+import { mockAgent } from "@indiekit-test/mock-agent";
+import { mastodon } from "../../lib/mastodon.js";
+
+await mockAgent("syndicator-mastodon");
+
+describe("syndicator-mastodon/lib/mastodon", () => {
+  let context;
+
+  beforeEach(() => {
+    context = {
+      me: "https://website.example",
+      options: {
+        accessToken: "token",
+        characterLimit: 500,
+        includePermalink: false,
+        serverUrl: "https://mastodon.example",
+      },
+      photo: [
+        {
+          url: "https://website.example/photo1.jpg",
+          alt: "Example image",
+        },
+      ],
+      statusId: "1234567890987654321",
+      statusParameters: { status: "Toot" },
+      statusUrl: "https://mastodon.example/@username/1234567890987654321",
+      statusUrlNotFound: "https://mastodon.example/@username/404",
+    };
+  });
+
+  it("Posts a favourite", async () => {
+    const { options, statusId, statusUrl } = context;
+    const result = await mastodon(options).postFavourite(statusUrl);
+
+    assert.equal(result, `https://mastodon.example/@username/${statusId}`);
+  });
+
+  it("Throws error posting a favourite", async () => {
+    const { options, statusUrlNotFound } = context;
+
+    await assert.rejects(mastodon(options).postFavourite(statusUrlNotFound), {
+      message: "Record not found",
+    });
+  });
+
+  it("Posts a reblog", async () => {
+    const { options, statusId, statusUrl } = context;
+    const result = await mastodon(options).postReblog(statusUrl);
+
+    assert.equal(result, `https://mastodon.example/@username/${statusId}`);
+  });
+
+  it("Throws error posting a reblog", async () => {
+    const { options, statusUrlNotFound } = context;
+
+    await assert.rejects(mastodon(options).postReblog(statusUrlNotFound), {
+      message: "Record not found",
+    });
+  });
+
+  it("Posts a status", async () => {
+    const { options, statusParameters, statusId } = context;
+    const result = await mastodon(options).postStatus(statusParameters);
+
+    assert.equal(result, `https://mastodon.example/@username/${statusId}`);
+  });
+
+  it("Throws error posting a status", async () => {
+    const { options, statusParameters } = context;
+    options.accessToken = "invalid";
+
+    await assert.rejects(mastodon(options).postStatus(statusParameters), {
+      message: "The access token is invalid",
+    });
+  });
+
+  it("Throws error fetching media to upload", async () => {
+    const { me, options } = context;
+    await assert.rejects(
+      mastodon(options).uploadMedia({ url: `${me}/404.jpg` }, me),
+      {
+        message: "Not Found",
+      },
+    );
+  });
+
+  it("Returns false passing an object to media upload function", async () => {
+    const { me, options } = context;
+    const result = await mastodon(options).uploadMedia({ foo: "bar" }, me);
+
+    assert.equal(result, undefined);
+  });
+
+  it("Posts a favourite of a Mastodon status to Mastodon", async () => {
+    const { me, options, statusUrl } = context;
+    const result = await mastodon(options).post({ "like-of": statusUrl }, me);
+
+    assert.equal(
+      result,
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+  });
+
+  it("Doesn’t post a favourite of a URL to Mastodon", async () => {
+    const { me, options } = context;
+    const result = await mastodon(options).post(
+      { "like-of": "https://foo.bar/lunchtime" },
+      me,
+    );
+
+    assert.equal(result, false);
+  });
+
+  it("Posts a repost of a Mastodon status to Mastodon", async () => {
+    const { me, options, statusUrl } = context;
+    const result = await mastodon(options).post({ "repost-of": statusUrl }, me);
+
+    assert.equal(
+      result,
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+  });
+
+  it("Doesn’t post a repost of a URL to Mastodon", async () => {
+    const { me, options } = context;
+    const result = await mastodon(options).post(
+      { "repost-of": "https://foo.bar/lunchtime" },
+      me,
+    );
+
+    assert.equal(result, false);
+  });
+
+  it("Posts a quote status to Mastodon", async () => {
+    const { me, options, statusUrl } = context;
+    const result = await mastodon(options).post(
+      {
+        content: {
+          html: "<p>Someone else who likes cheese sandwiches.</p>",
+        },
+        "repost-of": statusUrl,
+        "post-type": "repost",
+      },
+      me,
+    );
+
+    assert.equal(
+      result,
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+  });
+
+  it("Posts a status to Mastodon", async () => {
+    const { me, options } = context;
+    const result = await mastodon(options).post(
+      {
+        content: {
+          html: "<p>I ate a <em>cheese</em> sandwich, which was nice.</p>",
+          text: "I ate a cheese sandwich, which was nice.",
+        },
+        url: "https://foo.bar/lunchtime",
+      },
+      me,
+    );
+
+    assert.equal(
+      result,
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+  });
+
+  it("Posts a status with photo to Mastodon", async () => {
+    const { me, options, photo } = context;
+    const result = await mastodon(options).post(
+      {
+        content: {
+          html: "<p>Here’s the cheese sandwiches I ate.</p>",
+        },
+        photo,
+      },
+      me,
+    );
+
+    assert.equal(
+      result,
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+  });
+
+  it("Uploads media and returns a media id", async () => {
+    const { me, options, photo } = context;
+    const result = await mastodon(options).uploadMedia(photo[0], me);
+
+    assert.ok(result);
+  });
+});
---
+++ packages/syndicator-mastodon/test/unit/utils.js
@@ -0,0 +1,157 @@
+import { strict as assert } from "node:assert";
+import { describe, it } from "node:test";
+import { getFixture } from "@indiekit-test/fixtures";
+import {
+  createStatus,
+  getStatusIdFromUrl,
+  htmlToStatusText,
+} from "../../lib/utils.js";
+
+describe("syndicator-mastodon/lib/utils", () => {
+  it("Creates a status with article post name and URL", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/article-content-provided-html-text.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(
+      result.status,
+      "What I had for lunch https://foo.bar/lunchtime",
+    );
+  });
+
+  it("Creates a status that is unlisted", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/note-visibility-unlisted.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(result.status, "I ate a cheese sandwich, which was nice.");
+    assert.equal(result.visibility, "unlisted");
+  });
+
+  it("Creates a status with HTML content", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/note-content-provided-html.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(result.status, "> I ate a cheese sandwich, which was > 10.");
+  });
+
+  it("Creates a status with HTML content and appends last link", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/note-content-provided-html-with-link.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(
+      result.status,
+      "> I ate a cheese sandwich, which was > 10. https://en.wikipedia.org/wiki/Cheese",
+    );
+  });
+
+  it("Creates a status with HTML content and doesn’t append Mastodon link", () => {
+    const result = createStatus(
+      JSON.parse(
+        getFixture("jf2/note-content-provided-html-with-mastodon-link.jf2"),
+      ),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(result.status, "I ate @cheese’s sandwich, which was nice.");
+  });
+
+  it("Creates a reblog with status URL and post content", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/repost-mastodon.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(
+      result.status,
+      `Someone else who likes cheese sandwiches. https://mastodon.example/@username/1234567890987654321`,
+    );
+  });
+
+  it("Adds link to status post is in reply to", () => {
+    const result = createStatus(
+      JSON.parse(getFixture("jf2/reply-mastodon.jf2")),
+      {
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(result.status, "I ate a cheese sandwich too!");
+    assert.equal(result.inReplyToId, "1234567890987654321");
+  });
+
+  it("Throws creating a status if post is off-service reply", () => {
+    assert.throws(
+      () => {
+        createStatus(JSON.parse(getFixture("jf2/reply-off-service.jf2")), {
+          serverUrl: "https://mastodon.example",
+        });
+      },
+      {
+        name: "BadRequestError",
+        message: "Not a reply to a URL at this target",
+      },
+    );
+  });
+
+  it("Creates a status with a photo", () => {
+    const result = createStatus(
+      {
+        content: {
+          html: "<p>Here’s the cheese sandwich I ate.</p>",
+        },
+      },
+      {
+        mediaIds: ["1", "2", "3", "4"],
+        serverUrl: "https://mastodon.example",
+      },
+    );
+
+    assert.equal(result.status, "Here’s the cheese sandwich I ate.");
+    assert.deepEqual(result.mediaIds, ["1", "2", "3", "4"]);
+  });
+
+  it("Gets status ID from Mastodon permalink", () => {
+    const result = getStatusIdFromUrl(
+      "https://mastodon.example/@username/1234567890987654321",
+    );
+
+    assert.equal(result, "1234567890987654321");
+  });
+
+  it("Convert HTML to status text", () => {
+    const result = htmlToStatusText(
+      "<p>I ate a <em>cheese</em> sandwich, which was nice.</p>",
+      "https://mastodon.example",
+    );
+
+    assert.equal(result, "I ate a cheese sandwich, which was nice.");
+  });
+
+  it("Convert HTML to status text, appending last link href if present", () => {
+    const result = htmlToStatusText(
+      '<p>Hello <a href="/hello">world</a>, hello <a href="https://moon.example">moon</a>.</p>',
+      "https://mastodon.example",
+    );
+
+    assert.equal(result, "Hello world, hello moon. https://moon.example");
+  });
+});
