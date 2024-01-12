8316a08e0c66de296f5a539345255a98986729f4
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-01-12 17:21:50 +0000

    feat(frontend): event duration component

---
+++ packages/frontend/components/event-duration/index.js
@@ -0,0 +1,29 @@
+export const EventDurationComponent = class extends HTMLElement {
+  constructor() {
+    super();
+
+    this.allDayToggle = this.querySelector(`input[type="checkbox"]`);
+    this.dateTimeInputs = this.querySelectorAll(`input[type="datetime-local"]`);
+  }
+
+  connectedCallback() {
+    this.updateDateTimeInputs();
+  }
+
+  updateDateTimeInputs() {
+    for (const $dateInput of this.dateTimeInputs) {
+      $dateInput.type = this.allDayToggle.checked ? "date" : "datetime-local";
+
+      this.allDayToggle.addEventListener("click", () => {
+        const initialValue = $dateInput.value;
+        if (this.allDayToggle.checked) {
+          $dateInput.type = "date";
+          $dateInput.value = initialValue.split("T")[0];
+        } else {
+          $dateInput.type = "datetime-local";
+          $dateInput.value = `${initialValue}T12:30`;
+        }
+      });
+    }
+  }
+};
