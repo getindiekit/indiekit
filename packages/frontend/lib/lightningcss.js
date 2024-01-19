bae87d212813b8ca7648bb4e04df96aed0ab886c
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-01-19 18:18:24 +0000

    refactor(frontend): remove custom media

--- packages/frontend/lib/lightningcss.js
+++ packages/frontend/lib/lightningcss.js
@@ -4,9 +4,6 @@ import { bundleAsync } from "lightningcss";
 let { code } = await bundleAsync({
   filename: fileURLToPath(new URL("../styles/app.css", import.meta.url)),
   minify: true,
-  drafts: {
-    customMedia: true,
-  },
 });
 
 export const styles = () => code.toString();
