b2a78fbe172db28f304e2f12f7d9015739e691a9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-18 23:04:01 +0000

    refactor(util): pass options to formatDate

--- packages/util/lib/date.js
+++ packages/util/lib/date.js
@@ -6,13 +6,15 @@ import locales from "date-fns/locale/index.js";
  * Format a date
  * @param {string} string - ISO 8601 date
  * @param {string} tokens - Tokenised date format
- * @param {string} lang - ISO 639-1 (plus optional country code)
+ * @param {import("date-fns-tz").OptionsWithTZ} [options] - Options
  * @returns {string} Formatted date
  */
-export const formatDate = (string, tokens, lang = "en") => {
-  const locale = locales[lang.replace("-", "")];
+export const formatDate = (string, tokens, options = {}) => {
+  const locale = options.locale || "en";
+  options.locale = locales[String(locale).replace("-", "")];
+
   const date = string === "now" ? new Date() : parseISO(string);
-  const dateTime = dateFnsTz.format(date, tokens, { locale });
+  const dateTime = dateFnsTz.format(date, tokens, options);
   return dateTime;
 };
 
