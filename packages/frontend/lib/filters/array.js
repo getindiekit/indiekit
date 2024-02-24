41060db435c958a9d7a144b40033e5f60539320b
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-24 23:36:46 +0000

    feat(frontend): isArray filter

---
+++ packages/frontend/lib/filters/array.js
@@ -0,0 +1,6 @@
+/**
+ * Check if value is an array
+ * @param {*} value - Value to check
+ * @returns {boolean} Value is an array
+ */
+export const isArray = (value) => Array.isArray(value);
--- packages/frontend/lib/filters/index.js
+++ packages/frontend/lib/filters/index.js
@@ -5,6 +5,7 @@ export {
   slugify,
 } from "@indiekit/util";
 export { default as widont } from "widont";
+export { isArray } from "./array.js";
 export { languageName, languageNativeName } from "./locale.js";
 export { excerpt, includes, linkTo, markdown } from "./string.js";
 export { friendlyUrl, imageUrl } from "./url.js";
