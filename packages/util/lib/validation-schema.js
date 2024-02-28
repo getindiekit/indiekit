73f4b606db6e9d283097692daaa0818c1963cdee
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-28 22:29:42 +0000

    test(util): validation schema isRequired method

--- packages/util/lib/validation-schema.js
+++ packages/util/lib/validation-schema.js
@@ -1,3 +1,9 @@
+/**
+ * Check if field is required
+ * @param {import("express").Request} request - Request
+ * @param {string} field - Field name
+ * @returns {boolean} Field is required
+ */
 export const isRequired = (request, field) => {
   const { postTypes } = request.app.locals.publication;
   const postTypeConfig = postTypes[request.body.postType];
---
+++ packages/util/test/unit/validation-schema.js
@@ -0,0 +1,29 @@
+import { strict as assert } from "node:assert";
+import { describe, it } from "node:test";
+import { mockRequest } from "mock-req-res";
+import { isRequired } from "../../lib/validation-schema.js";
+
+describe("util/lib/validation-schema", () => {
+  it("Check if field is required", () => {
+    const request = mockRequest({
+      app: {
+        locals: {
+          publication: {
+            postTypes: {
+              foobar: {
+                type: "foobar",
+                name: "Foobar",
+                properties: ["foo", "bar"],
+                "required-properties": ["foo"],
+              },
+            },
+          },
+        },
+      },
+      body: { postType: "foobar" },
+    });
+
+    assert.equal(isRequired(request, "foo"), true);
+    assert.equal(isRequired(request, "bar"), false);
+  });
+});
