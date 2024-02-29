176fc47cf42e575a157ed7c6c61e3d7780e782e9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-02-29 23:18:29 +0000

    fix(endpoint-auth): only show error if message

--- packages/endpoint-auth/lib/controllers/documentation.js
+++ packages/endpoint-auth/lib/controllers/documentation.js
@@ -6,7 +6,7 @@ export const documentationController = (error, request, response, next) => {
   if (request.accepts("html")) {
     response.render("auth", {
       title: response.locals.__("auth.guidance.title"),
-      error,
+      ...(error.message && { error }),
     });
   } else if (request.accepts("json")) {
     return next(error);
