3b65f23a82f76535bbf96a7c7b09b3ea27c1558d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-03-24 16:20:08 +0000

    feat(endpoint-auth): show requested scopes and indicate which are supported

--- packages/endpoint-auth/lib/scope.js
+++ packages/endpoint-auth/lib/scope.js
@@ -1,4 +1,24 @@
-export const scopes = ["create", "update", "draft", "media", "delete"];
+export const scopes = {
+  // IndieAuth scopes
+  email: { supported: false },
+  profile: { supported: false },
+  // Micropub scopes
+  create: { supported: true },
+  draft: { supported: true },
+  update: { supported: true },
+  delete: { supported: true },
+  media: { supported: true },
+  // Microsub scopes
+  read: { supported: true },
+  follow: { supported: true },
+  mute: { supported: true },
+  block: { supported: true },
+  channels: { supported: true },
+};
+
+export const supportedScopes = Object.entries(scopes)
+  .filter(([, value]) => value.supported)
+  .map(([key]) => key);
 
 /**
  * Get `items` object for checkboxes component
@@ -7,9 +27,18 @@ export const scopes = ["create", "update", "draft", "media", "delete"];
  * @returns {object} Items for checkboxes component
  */
 export function getScopeItems(scope, response) {
-  return scopes.map((value) => ({
-    label: response.locals.__(`scope.${value}.label`),
+  const localisedScopes = Object.keys(scopes);
+  const requestedScopes = scope ? scope.split(" ") : [];
+
+  return requestedScopes.map((value) => ({
+    label: localisedScopes.includes(value)
+      ? response.locals.__(`scope.${value}.label`)
+      : value,
+    checked: supportedScopes.includes(value) && requestedScopes.includes(value),
+    ...(!supportedScopes.includes(value) && {
+      disabled: true,
+      hint: response.locals.__("scope.notSupported.hint"),
+    }),
     value,
-    checked: scope?.includes(value),
   }));
 }
