d9aa2062b29e4be01ec270e6e4e1ed87610b93f9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-02 00:02:41 +0100

    fix(frontend): use browser exports in bundle

--- packages/frontend/lib/rollup.js
+++ packages/frontend/lib/rollup.js
@@ -7,7 +7,7 @@ export const scripts = async () => {
   const bundle = await rollup({
     input: fileURLToPath(new URL("../scripts/app.js", import.meta.url)),
     // @ts-ignore https://github.com/rollup/plugins/issues/1329
-    plugins: [commonjs(), nodeResolve({ preferBuiltins: true })],
+    plugins: [commonjs(), nodeResolve({ browser: true, preferBuiltins: true })],
   });
 
   const { output } = await bundle.generate({ format: "iife", name: "app" });
