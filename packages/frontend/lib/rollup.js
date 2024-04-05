558f546ab923f705412bee843d91ce203694cb03
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-05 14:25:15 +0100

    feat(frontend): minify javascript bundle

--- packages/frontend/lib/rollup.js
+++ packages/frontend/lib/rollup.js
@@ -2,12 +2,17 @@ import { fileURLToPath } from "node:url";
 import { rollup } from "rollup";
 import commonjs from "@rollup/plugin-commonjs";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
+import { minify } from "rollup-plugin-esbuild";
 
 export const scripts = async () => {
   const bundle = await rollup({
     input: fileURLToPath(new URL("../scripts/app.js", import.meta.url)),
     // @ts-ignore https://github.com/rollup/plugins/issues/1329
-    plugins: [commonjs(), nodeResolve({ browser: true, preferBuiltins: true })],
+    plugins: [
+      commonjs(),
+      nodeResolve({ browser: true, preferBuiltins: true }),
+      minify(),
+    ],
   });
 
   const { output } = await bundle.generate({ format: "iife", name: "app" });
