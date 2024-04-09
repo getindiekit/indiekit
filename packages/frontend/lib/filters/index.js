60d4deca1e69cda2fe0fc415d2b2fab7c70258e9
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-09 14:45:10 +0100

    feat(frontend): replace widont filter with text-wrap balance

--- packages/frontend/lib/filters/index.js
+++ packages/frontend/lib/filters/index.js
@@ -4,7 +4,6 @@ export {
   getCanonicalUrl as url,
   slugify,
 } from "@indiekit/util";
-export { default as widont } from "widont";
 export { isArray } from "./array.js";
 export { languageName, languageNativeName } from "./locale.js";
 export { excerpt, includes, linkTo, markdown } from "./string.js";
