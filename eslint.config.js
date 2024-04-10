ffce11e023325aaf83d4840cdab48afaa2954918
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-10 01:17:23 +0100

    ci: lint web components

--- eslint.config.js
+++ eslint.config.js
@@ -2,12 +2,14 @@ import js from "@eslint/js";
 import prettier from "eslint-config-prettier";
 import jsdoc from "eslint-plugin-jsdoc";
 import unicorn from "eslint-plugin-unicorn";
+import webComponents from "eslint-plugin-wc";
 import globals from "globals";
 
 export default [
   js.configs.recommended,
   jsdoc.configs["flat/recommended"],
   unicorn.configs["flat/recommended"],
+  webComponents.configs["flat/recommended"],
   {
     ignores: ["**/.cache/", "_site", "docs/.vitepress/config.js"],
   },
@@ -19,6 +21,7 @@ export default [
   {
     files: ["**/frontend/**/*.js"],
     languageOptions: { globals: { ...globals.browser } },
+    rules: { "wc/no-self-class": "warn" },
   },
   {
     files: ["**/test/**/*.js"],
