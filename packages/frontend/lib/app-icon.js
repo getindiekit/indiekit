3245561ade915c76ee329df95d861c50ac507bcd
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-14 14:53:19 +0100

    feat(frontend): update app icon function to take purpose

--- packages/frontend/lib/app-icon.js
+++ packages/frontend/lib/app-icon.js
@@ -2,15 +2,22 @@ import fs from "node:fs";
 import { fileURLToPath } from "node:url";
 import sharp from "sharp";
 
-export const appIcon = async (themeColor, size) => {
+/**
+ * Get application icon image
+ * @param {string|number} size - Icon size
+ * @param {string} themeColor - Theme colour
+ * @param {string} [purpose] - Icon purpose (any, maskable or monochrome)
+ * @returns {Promise<Buffer>} File buffer
+ */
+export const appIcon = async (size, themeColor, purpose = "any") => {
   const svgPath = fileURLToPath(
-    new URL("../assets/app-icon.svg", import.meta.url),
+    new URL(`../assets/app-icon-${purpose}.svg`, import.meta.url),
   );
 
   const svg = fs.readFileSync(svgPath);
   return sharp(svg)
     .tint(themeColor)
-    .resize(size)
+    .resize(Number(size))
     .png({ colours: 16 })
     .toBuffer();
 };
