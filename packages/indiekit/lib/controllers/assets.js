6cc7f2ec2e3e4d43060bb122658720288f2eea2d
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-04-14 16:04:08 +0100

    feat(indiekit): shortcut icons

--- packages/indiekit/lib/controllers/assets.js
+++ packages/indiekit/lib/controllers/assets.js
@@ -1,4 +1,4 @@
-import { appIcon, scripts, styles } from "@indiekit/frontend";
+import { appIcon, shortcutIcon, scripts, styles } from "@indiekit/frontend";
 
 export const getAppIcon = async (request, response, next) => {
   const { purpose, size } = request.params;
@@ -6,6 +6,17 @@ export const getAppIcon = async (request, response, next) => {
 
   try {
     const png = await appIcon(size, themeColor, purpose);
+    return response.type("image/png").send(png).end();
+  } catch {
+    next();
+  }
+};
+
+export const getShortcutIcon = async (request, response, next) => {
+  const { name, size } = request.params;
+
+  try {
+    const png = await shortcutIcon(size, name);
     return response.type("image/png").send(png).end();
   } catch {
     next();
---
+++ packages/indiekit/test/integration/200-shortcut-icon.js
@@ -0,0 +1,20 @@
+import { strict as assert } from "node:assert";
+import { after, describe, it } from "node:test";
+import supertest from "supertest";
+import { testServer } from "@indiekit-test/server";
+
+const server = await testServer();
+const request = supertest.agent(server);
+
+describe("indiekit GET /assets/shortcut-icon-96-tick.png", () => {
+  it("Returns JavaScript", async () => {
+    const result = await request.get("/assets/shortcut-icon-96-tick.png");
+
+    assert.equal(result.status, 200);
+    assert.equal(result.type, "image/png");
+  });
+
+  after(() => {
+    server.close(() => process.exit(0));
+  });
+});
