845127517c9070a405269170662467b2588dea44
Giacomo Debidda <giacomo@giacomodebidda.com>
2024-10-22 20:20:49 +0200

    feat(frontend): use esbuild for bundling

--- packages/frontend/index.js
+++ packages/frontend/index.js
@@ -3,8 +3,6 @@ import { fileURLToPath } from "node:url";
 export const assetsPath = fileURLToPath(new URL("assets", import.meta.url));
 export { appIcon, shortcutIcon } from "./lib/sharp.js";
 export { templates } from "./lib/nunjucks.js";
-export { styles } from "./lib/lightningcss.js";
-export { scripts } from "./lib/rollup.js";
 export { getBackgroundColor, getThemeColor } from "./lib/utils/theme.js";
 
 export { tagInputSanitizer } from "./components/tag-input/sanitizer.js";
