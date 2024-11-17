9045997ebb499b8358d615a563a9b396780d2620
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2024-11-17 14:17:14 +0000

    feat(frontend): replace rollup with esbuild

---
+++ packages/frontend/lib/esbuild.js
@@ -0,0 +1,21 @@
+import { fileURLToPath } from "node:url";
+import * as esbuild from "esbuild";
+
+export const scripts = async () => {
+  const inputFile = fileURLToPath(
+    new URL("../scripts/app.js", import.meta.url),
+  );
+
+  const result = await esbuild.build({
+    entryPoints: [inputFile],
+    bundle: true,
+    legalComments: "none",
+    minify: true,
+    write: false,
+  });
+
+  const { contents } = result.outputFiles[0];
+  const code = new TextDecoder().decode(contents);
+
+  return code;
+};
