3db3f9f167ea224cf632961a6e1a33c11ea563c0
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-09-12 20:57:25 +0100

    docs(error): document error methods

--- packages/error/index.js
+++ packages/error/index.js
@@ -1,14 +1,39 @@
 import { errors } from "./errors.js";
 
+/**
+ * @typedef {object} IndiekitErrorOptions
+ * @property {string} [code] - Error code, i.e. `not_found`
+ * @property {unknown} [cause] - Error that caused this error
+ * @property {string} [plugin] - Name of plug-in, prefixed to the message
+ * @property {string} [scope] - Scope required to perform the request
+ * @property {number} [status] - HTTP status code
+ * @property {string} [uri] - URL of a page describing the error
+ */
+
 export class IndiekitError extends Error {
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Bad request error
+   */
   static badRequest(message, options) {
     return new IndiekitError(message, { ...options, code: "bad_request" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Forbidden error
+   */
   static forbidden(message, options) {
     return new IndiekitError(message, { ...options, code: "forbidden" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Insufficient scope error
+   */
   static insufficientScope(message, options) {
     return new IndiekitError(message, {
       ...options,
@@ -16,22 +41,47 @@ export class IndiekitError extends Error {
     });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Invalid request error
+   */
   static invalidRequest(message, options) {
     return new IndiekitError(message, { ...options, code: "invalid_request" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Not found error
+   */
   static notFound(message, options) {
     return new IndiekitError(message, { ...options, code: "not_found" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Not implemented error
+   */
   static notImplemented(message, options) {
     return new IndiekitError(message, { ...options, code: "not_implemented" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Unauthorized error
+   */
   static unauthorized(message, options) {
     return new IndiekitError(message, { ...options, code: "unauthorized" });
   }
 
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   * @returns {IndiekitError} Unsupported media type error
+   */
   static unsupportedMediaType(message, options) {
     return new IndiekitError(message, {
       ...options,
@@ -39,6 +89,11 @@ export class IndiekitError extends Error {
     });
   }
 
+  /**
+   * Create an error from an unsuccessful fetch response
+   * @param {Response} response - Fetch response
+   * @returns {Promise<IndiekitError>} Indiekit error
+   */
   static async fromFetch(response) {
     let body;
     let message = response.statusText;
@@ -58,23 +113,12 @@ export class IndiekitError extends Error {
     });
   }
 
-  constructor(message, options = {}) {
-    super(message, options);
-    this.message = options.plugin ? `${options.plugin}: ${message}` : message;
-    this.code = options.code || "indiekit";
-    this.name = this.getError(this.code)?.name || this.name;
-    this.status = options.status || this.getError(this.code)?.status || 500;
-
-    if (options.scope) {
-      this.scope = options.scope;
-    }
-
-    if (options.uri) {
-      this.uri = options.uri;
-    }
-  }
-
-  getError(name) {
+  /**
+   * Get status and name for a given error code
+   * @param {string} [name] - Error code or name
+   * @returns {{ code: string, name: string, status: number }|undefined} Error values
+   */
+  static getError(name) {
     const code = String(name || "unknown")
       .replaceAll(" ", "_")
       .toLowerCase();
@@ -83,5 +127,26 @@ export class IndiekitError extends Error {
     if (!error) return;
 
     return { ...error, code };
+  }
+
+  /**
+   * @param {string} message - Error message
+   * @param {IndiekitErrorOptions} [options] - Error options
+   */
+  constructor(message, options = {}) {
+    super(message, options);
+    this.message = options.plugin ? `${options.plugin}: ${message}` : message;
+    this.code = options.code || "indiekit";
+    this.name = IndiekitError.getError(this.code)?.name || this.name;
+    this.status =
+      options.status || IndiekitError.getError(this.code)?.status || 500;
+
+    if (options.scope) {
+      this.scope = options.scope;
+    }
+
+    if (options.uri) {
+      this.uri = options.uri;
+    }
   }
 }
