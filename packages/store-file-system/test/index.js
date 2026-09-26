194060aa6cec97182785c28cfa0e56f82d4db9a6
Paul Robert Lloyd <me+git@paulrobertlloyd.com>
2026-09-26 20:27:46 +0100

    test(store-file-system): replace mock-fs with temporary directory

--- packages/store-file-system/test/index.js
+++ packages/store-file-system/test/index.js
@@ -1,26 +1,31 @@
 import { strict as assert } from "node:assert";
 import { existsSync } from "node:fs";
 import fs from "node:fs/promises";
-import { afterEach, describe, it } from "node:test";
+import os from "node:os";
+import path from "node:path";
+import { afterEach, beforeEach, describe, it } from "node:test";
 
 import { Indiekit } from "@indiekit/indiekit";
-import mockFs from "mock-fs";
 
 import FileSystemStore from "../index.js";
 
 describe("store-file-system", () => {
-  const fileSystem = new FileSystemStore({
-    directory: "directory",
+  let directory;
+  let fileSystem;
+
+  beforeEach(async () => {
+    directory = await fs.mkdtemp(path.join(os.tmpdir(), "indiekit-"));
+    fileSystem = new FileSystemStore({ directory });
   });
 
-  afterEach(() => {
-    mockFs.restore();
+  afterEach(async () => {
+    await fs.rm(directory, { force: true, recursive: true });
   });
 
   it("Gets plug-in info", () => {
     assert.equal(fileSystem.name, "File system store");
-    assert.equal(fileSystem.info.name, "directory");
-    assert.equal(fileSystem.info.uid, "file://directory");
+    assert.equal(fileSystem.info.name, directory);
+    assert.equal(fileSystem.info.uid, `file://${directory}`);
   });
 
   it("Gets plug-in installation prompts", () => {
@@ -35,26 +40,27 @@ describe("store-file-system", () => {
       config: {
         plugins: ["@indiekit/store-file-system"],
         publication: { me: "https://website.example" },
-        "@indiekit/store-file-system": { directory: "directory" },
+        "@indiekit/store-file-system": { directory },
       },
     });
     await indiekit.installPlugins();
     await indiekit.updatePublicationConfig();
 
-    assert.equal(indiekit.publication.store.info.name, "directory");
+    assert.equal(indiekit.publication.store.info.name, directory);
   });
 
   it("Creates file", async () => {
-    mockFs();
-
     const result = await fileSystem.createFile("foo.txt", "foo");
 
-    assert.equal(result, "file://directory/foo.txt");
-    assert.equal(await fs.readFile("directory/foo.txt", "utf8"), "foo");
+    assert.equal(result, `file://${directory}/foo.txt`);
+    assert.equal(
+      await fs.readFile(path.join(directory, "foo.txt"), "utf8"),
+      "foo",
+    );
   });
 
   it("Doesn’t create file if already exists", async () => {
-    mockFs({ "directory/bar.txt": "foo" });
+    await fs.writeFile(path.join(directory, "bar.txt"), "foo");
 
     const result = await fileSystem.createFile("bar.txt", "foo");
 
@@ -62,57 +68,54 @@ describe("store-file-system", () => {
   });
 
   it("Throws error creating file", async () => {
-    mockFs();
-
     await assert.rejects(fileSystem.createFile(undefined, "foo"), {
       message: `File system store: The "path" argument must be of type string. Received undefined`,
     });
   });
 
   it("Updates file", async () => {
-    mockFs({ "directory/foo.txt": "foo" });
+    await fs.writeFile(path.join(directory, "foo.txt"), "foo");
 
     const result = await fileSystem.updateFile("foo.txt", "bar");
 
-    assert.equal(result, "file://directory/foo.txt");
-    assert.equal(await fs.readFile("directory/foo.txt", "utf8"), "bar");
+    assert.equal(result, `file://${directory}/foo.txt`);
+    assert.equal(
+      await fs.readFile(path.join(directory, "foo.txt"), "utf8"),
+      "bar",
+    );
   });
 
   it("Updates and renames file", async () => {
-    mockFs({ "directory/foo.txt": "foo" });
+    await fs.writeFile(path.join(directory, "foo.txt"), "foo");
 
     const result = await fileSystem.updateFile("foo.txt", "bar", {
       newPath: "bar.txt",
     });
 
-    assert.equal(result, "file://directory/bar.txt");
-    assert.equal(await fs.readFile("directory/bar.txt", "utf8"), "bar");
+    assert.equal(result, `file://${directory}/bar.txt`);
+    assert.equal(
+      await fs.readFile(path.join(directory, "bar.txt"), "utf8"),
+      "bar",
+    );
   });
 
   it("Throws error updating file", async () => {
-    mockFs();
-
-    await assert.rejects(fileSystem.updateFile("foo.txt", "foo"), {
-      message:
-        "File system store: ENOENT, no such file or directory 'directory/foo.txt'",
+    await assert.rejects(fileSystem.updateFile("missing/foo.txt", "foo"), {
+      message: /^File system store: ENOENT: no such file or directory/,
     });
   });
 
   it("Deletes file", async () => {
-    mockFs({ "directory/foo.txt": "foo" });
+    await fs.writeFile(path.join(directory, "foo.txt"), "foo");
 
     await fileSystem.deleteFile("foo.txt");
-    const result = existsSync("directory/foo.txt");
 
-    assert.equal(result, false);
+    assert.equal(existsSync(path.join(directory, "foo.txt")), false);
   });
 
   it("Throws error deleting file", async () => {
-    mockFs();
-
     await assert.rejects(fileSystem.deleteFile("foo.txt"), {
-      message:
-        "File system store: ENOENT, no such file or directory 'directory/foo.txt'",
+      message: /^File system store: ENOENT: no such file or directory/,
     });
   });
 });
